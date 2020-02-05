d476f0e4d9fec050875d749e4e3f920b77c1e811
Adam Silver <adambsilver@gmail.com>
2020-02-05 15:39:27 +0000

    Different offer so far

--- app/routes/application.js
+++ app/routes/application.js
@@ -135,8 +135,8 @@ module.exports = router => {
     const applicationId = req.params.applicationId
     const { decision } = req.body
 
-    if (decision === 'different-offer') {
-      res.redirect(`/application/${applicationId}/different-offer`)
+    if (decision === 'different-course') {
+      res.redirect(`/application/${applicationId}/different-course`)
     } else {
       res.redirect(`/application/${applicationId}/withdraw`)
     }
@@ -288,4 +288,45 @@ module.exports = router => {
       applicationId: req.params.applicationId
     })
   })
+
+  router.get('/application/:applicationId/different-course', (req, res) => {
+    res.render(`application/different-course`, {
+      applicationId: req.params.applicationId
+    })
+  })
+
+  // post comments about withdrawing
+  router.post('/application/:applicationId/different-course', (req, res) => {
+    const applicationId = req.params.applicationId
+    const application = req.session.data.applications[applicationId]
+
+    var changing = req.body['course-change'];
+
+    if (changing === 'provider') {
+      res.redirect(`/application/${applicationId}/different-course/provider`)
+    } else if (changing === 'course') {
+      res.redirect(`/application/${applicationId}/different-course/course`)
+    } else if (changing === 'location') {
+      res.redirect(`/application/${applicationId}/different-course/location`)
+    }
+  })
+
+  router.get('/application/:applicationId/different-course/provider', (req, res) => {
+    res.render(`application/different-course--provider`, {
+      applicationId: req.params.applicationId
+    })
+  })
+
+  router.get('/application/:applicationId/different-course/course', (req, res) => {
+    res.render(`application/different-course--course`, {
+      applicationId: req.params.applicationId
+    })
+  })
+
+  router.get('/application/:applicationId/different-course/location', (req, res) => {
+    res.render(`application/different-course--course`, {
+      applicationId: req.params.applicationId
+    })
+  })
+
 }
