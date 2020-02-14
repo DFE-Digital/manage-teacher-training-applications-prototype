1c00f41ecf2bd9da6c032229e80ab2b01ac305f8
Adam Silver <adambsilver@gmail.com>
2020-02-14 10:14:46 +0000

    Laying out conditions and stuff

--- app/routes/make-offer.js
+++ app/routes/make-offer.js
@@ -18,8 +18,34 @@ module.exports = router => {
     const applicationId = req.params.applicationId
     const application = req.session.data.applications[applicationId]
 
+    let standardConditions = req.session.data['standard-conditions'].map((item) => {
+      return {
+        description: item
+      }
+    })
+
+    let furtherConditions = [];
+
+    if (req.session.data['condition-1']) {
+      furtherConditions.push({ id: uuid(), description: req.session.data['condition-1'], met: false })
+    }
+    if (req.session.data['condition-2']) {
+      furtherConditions.push({ id: uuid(), description: req.session.data['condition-2'], met: false })
+    }
+    if (req.session.data['condition-3']) {
+      furtherConditions.push({ id: uuid(), description: req.session.data['condition-3'], met: false })
+    }
+    if (req.session.data['condition-4']) {
+      furtherConditions.push({ id: uuid(), description: req.session.data['condition-4'], met: false })
+    }
+
+    let recommendations = req.session.data.recommendations
+
     res.render('application/confirm-offer', {
-      applicationId: req.params.applicationId
+      applicationId: req.params.applicationId,
+      standardConditions,
+      furtherConditions,
+      recommendations
     })
   })
 
