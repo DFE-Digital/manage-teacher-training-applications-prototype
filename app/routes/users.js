2efbf18a0d5aff9b4024b3ff95fbaecbd0bedbf6
Adam Silver <adambsilver@gmail.com>
2020-03-31 16:27:56 +0100

    Add show page and edit name page

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,22 +1,30 @@
+const utils = require( '../data/application-utils')
+
 module.exports = router => {
 
   router.get('/users', (req, res) => {
 
-    let flash;
-
-    // real flash
-    flash = (req.flash('success')[0] == 'user-invited') ? "User successfully invited" : null
-
-    // fake flash in query string for taking screenshots
-    flash = req.query.flash == 'user-invited' ? "User successfully invited" : null
+    const flashMessage = utils.getFlashMessage({
+      flash: req.flash('success'),
+      overrideValue: req.query.flash,
+      map: {
+        'user-invited': 'User successfully invited',
+        'user-name-updated': 'User’s name successfully updated'
+      }
+    })
 
     res.render('users/index', {
-      flash: flash
+      flashMessage: flashMessage
     })
   })
 
   router.post('/users/new', (req, res) => {
     req.flash('success', 'user-invited');
+    res.redirect(`/users`);
+  })
+
+  router.post('/users/change-name', (req, res) => {
+    req.flash('success', 'user-name-updated');
     res.redirect(`/users`);
   })
 
