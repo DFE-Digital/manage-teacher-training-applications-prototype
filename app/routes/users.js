fb628ab2f8f90cc058fecc87e62d685df86ce4ff
Adam Silver <adambsilver@gmail.com>
2020-04-01 12:01:06 +0100

    Flows for managing users

--- app/routes/users.js
+++ app/routes/users.js
@@ -9,11 +9,28 @@ module.exports = router => {
       overrideValue: req.query.flash,
       map: {
         'user-invited': 'User successfully invited',
-        'user-name-updated': 'User’s name successfully updated'
+        'user-account-deleted': 'User’s account successfully deleted'
       }
     })
 
     res.render('users/index', {
+      flashMessage: flashMessage
+    })
+  })
+
+  router.get('/users/show', (req, res) => {
+
+    const flashMessage = utils.getFlashMessage({
+      flash: req.flash('success'),
+      overrideValue: req.query.flash,
+      map: {
+        'user-name-updated': 'User’s name successfully updated',
+        'user-email-address-updated': 'User’s email address successfully updated',
+        'user-permissions-updated': 'User’s permissions successfully updated'
+      }
+    })
+
+    res.render('users/show', {
       flashMessage: flashMessage
     })
   })
@@ -25,6 +42,22 @@ module.exports = router => {
 
   router.post('/users/change-name', (req, res) => {
     req.flash('success', 'user-name-updated');
+    res.redirect(`/users/show`);
+  })
+
+
+  router.post('/users/change-email-address', (req, res) => {
+    req.flash('success', 'user-email-address-updated');
+    res.redirect(`/users/show`);
+  })
+
+  router.post('/users/change-permissions', (req, res) => {
+    req.flash('success', 'user-permissions-updated');
+    res.redirect(`/users/show`);
+  })
+
+  router.post('/users/delete', (req, res) => {
+    req.flash('success', 'user-account-deleted');
     res.redirect(`/users`);
   })
 
