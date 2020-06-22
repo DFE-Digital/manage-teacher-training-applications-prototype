a50455bf175e2ceafcc7ff867d3795d9415197c3
Adam Silver <adambsilver@gmail.com>
2020-06-22 08:20:37 +0100

    Cycle labelling and copy style

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,7 +2,7 @@ const applications = require('./applications')
 
 module.exports = {
   applications,
-  cycle: "2019-20",
+  cycle: "2020-2021 (current cycle)",
   bare: process.env.BARE,
   flags: {
     interview_preferences: true
