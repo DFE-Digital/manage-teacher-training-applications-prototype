3efe3cea257065cb6e7c161f8b208ff5fb281b89
Adam Silver <adambsilver@gmail.com>
2020-07-08 14:48:21 +0100

    Add sort default

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -3,6 +3,7 @@ const applications = require('./applications')
 module.exports = {
   applications,
   cycle: "Current cycle (2020 to 2021)",
+  sortby: 'needs most attention',
   bare: process.env.BARE,
   flags: {
     interview_preferences: true
