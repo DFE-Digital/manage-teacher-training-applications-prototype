52d1d1988d357af10a3476c4fc7c3d2eccbc55a7
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-07-20 17:50:13 +0100

    Make nationaity in personal details an array

--- app/data/generators/personal-details.js
+++ app/data/generators/personal-details.js
@@ -2,6 +2,6 @@ module.exports = (faker) => ({
   'given-name': faker.name.firstName(),
   'family-name': faker.name.lastName(),
   'date-of-birth': faker.date.between('1958-01-01', '1998-01-01'),
-  nationality: 'British',
+  nationality: ['British'],
   'second-nationality': false
 })
