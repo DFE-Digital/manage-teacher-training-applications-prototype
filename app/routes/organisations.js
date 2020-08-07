10b8cbe47dc3f801b631f514c21c085263f5d4f4
Adam Silver <adambsilver@gmail.com>
2020-08-07 14:47:06 +0100

    Fix user pages

--- app/routes/organisations.js
+++ app/routes/organisations.js
@@ -10,43 +10,27 @@ module.exports = router => {
       return relationship.org.id == org.id
     })
 
+    const flashMessage = utils.getFlashMessage({
+      flash: req.flash('success'),
+      overrideValue: req.query.flash,
+      map: {
+        'permissions-changed': 'Permissions successfully changed'
+      }
+    })
+
     res.render('organisations/show', {
+      flashMessage,
       org,
       relationships
     })
   })
 
-  router.get('/organisations/:organisationId/relationship/:relationshipId/edit', (req, res) => {
-
-    // let org = req.session.data.user.organisations.find(org => org.id == req.params.organisationId)
-
-    // let relationships = req.session.data.relationships.filter(relationship => {
-    //   return relationship.org.id == org.id
-    // })
-
-    // res.render('organisations/edit', {
-    //   org,
-    //   relationships
-    // })
+  router.get('/organisations/:organisationId/edit', (req, res) => {
+    res.render('organisations/edit')
   })
 
-
-  // router.get('/organisations/show3', (req, res) => {
-  //   const flashMessage = utils.getFlashMessage({
-  //     flash: req.flash('success'),
-  //     overrideValue: req.query.flash,
-  //     map: {
-  //       'permissions-changed': 'Permissions successfully changed'
-  //     }
-  //   })
-
-  //   res.render('organisations/show3', {
-  //     flashMessage: flashMessage
-  //   })
-  // })
-
-  // router.post('/organisations/edit', (req, res) => {
-  //   // req.flash('success', 'permissions-changed')
-  //   res.redirect('/organisations/show3')
-  // })
+  router.post('/organisations/:organisationId/edit', (req, res) => {
+    req.flash('success', 'permissions-changed')
+    res.redirect(`/organisations/${req.params.organisationId}`)
+  })
 }
