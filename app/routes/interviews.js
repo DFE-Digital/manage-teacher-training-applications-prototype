280d11447186ae1bdf075ae5e7d90b949083983c
Adam Silver <adambsilver@gmail.com>
2020-09-23 11:00:51 +0100

    Edit view created

--- app/routes/interviews.js
+++ app/routes/interviews.js
@@ -2,6 +2,43 @@ const utils = require('../data/application-utils')
 const { v4: uuidv4 } = require('uuid')
 const { DateTime } = require('luxon')
 
+function getTimeObject(time) {
+  var hours;
+  var mins;
+  var isAm = time.indexOf('am') > -1;
+  if(isAm) {
+    time = time.split('am')[0].trim()
+    if(time.indexOf(":") > -1) {
+      hours = time.split(":")[0]
+      mins = time.split(":")[1]
+    } else {
+      hours = time
+      min = "00"
+    }
+
+    // if they selected 12am
+    if(hours == "12") {
+      hours = "00"
+    }
+  } else {
+    time = time.split('pm')[0].trim()
+    if(time.indexOf(":") > -1) {
+      hours = time.split(":")[0]
+      mins = time.split(":")[1]
+    } else {
+      hours = time
+      mins = "00"
+    }
+
+    // convert to 24 hour only if not 12, if it's 12pm it's fine as 12
+    if(hours != "12") {
+      hours = parseInt(hours, 10) + 12;
+    }
+  }
+
+  return {hours, mins}
+}
+
 module.exports = router => {
   router.get('/interviews', (req, res) => {
     const apps = req.session.data.applications
@@ -75,46 +112,13 @@ module.exports = router => {
     const applicationId = req.params.applicationId
     const application = req.session.data.applications.find(app => app.id === applicationId)
 
-    application.status = 'Awaiting interview';
     application.interviews.items = application.interviews.items || [];
 
     var id = uuidv4();
 
-    var time;
-    var hours;
-    var mins;
-    var isAm = req.session.data.interview.time.indexOf('am') > -1;
-    if(isAm) {
-      time = req.session.data.interview.time.split('am')[0].trim()
-      if(time.indexOf(":") > -1) {
-        hours = time.split(":")[0]
-        mins = time.split(":")[1]
-      } else {
-        hours = time
-        min = "00"
-      }
+    var time = getTimeObject(req.session.data.interview.time);
 
-      // if they selected 12am
-      if(hours == "12") {
-        hours = "00"
-      }
-    } else {
-      time = req.session.data.interview.time.split('pm')[0].trim()
-      if(time.indexOf(":") > -1) {
-        hours = time.split(":")[0]
-        mins = time.split(":")[1]
-      } else {
-        hours = time
-        mins = "00"
-      }
-
-      // convert to 24 hour only if not 12, if it's 12pm it's fine as 12
-      if(hours != "12") {
-        hours = parseInt(hours, 10) + 12;
-      }
-    }
-
-    var date = DateTime.local(parseInt(req.session.data.interview.date.year, 10), parseInt(req.session.data.interview.date.month, 10), parseInt(req.session.data.interview.date.day, 10), parseInt(hours, 10), parseInt(mins, 10));
+    var date = DateTime.local(parseInt(req.session.data.interview.date.year, 10), parseInt(req.session.data.interview.date.month, 10), parseInt(req.session.data.interview.date.day, 10), parseInt(time.hours, 10), parseInt(time.mins, 10));
 
     application.interviews.items.push({
       id,
@@ -137,4 +141,61 @@ module.exports = router => {
     res.redirect(`/application/${applicationId}/interviews`)
 
   })
+
+  router.get('/application/:applicationId/interviews/:interviewId/edit', (req, res) => {
+    const applicationId = req.params.applicationId
+    const interviewId = req.params.interviewId
+    const application = req.session.data.applications.find(app => app.id === applicationId)
+
+    res.render('application/interviews/edit/index', {
+      application,
+      interview: application.interviews.items.find(interview => interview.id === interviewId)
+    })
+  })
+
+  router.post('/application/:applicationId/interviews/:interviewId/edit', (req, res) => {
+    res.redirect(`/application/${req.params.applicationId}/interviews/${req.params.interviewId}/edit/check`)
+  })
+
+  router.get('/application/:applicationId/interviews/:interviewId/edit/check', (req, res) => {
+    const applicationId = req.params.applicationId
+    const application = req.session.data.applications.find(app => app.id === applicationId)
+    const interviewId = req.params.interviewId
+
+    res.render('application/interviews/edit/check', {
+      application,
+      interview: application.interviews.items.find(interview => interview.id === interviewId)
+    })
+  })
+
+  router.post('/application/:applicationId/interviews/:interviewId/edit/check', (req, res) => {
+
+    const applicationId = req.params.applicationId
+    const interviewId = req.params.interviewId
+    const application = req.session.data.applications.find(app => app.id === applicationId)
+    const interview = application.interviews.items.find(interview => interview.id === interviewId)
+
+    var time = getTimeObject(req.session.data.interview.time);
+    var date = DateTime.local(parseInt(req.session.data.interview.date.year, 10), parseInt(req.session.data.interview.date.month, 10), parseInt(req.session.data.interview.date.day, 10), parseInt(time.hours, 10), parseInt(time.mins, 10));
+
+    interview.date = date.toISO()
+    interview.details = req.session.data.interview.details
+
+    application.events.items.push({
+      title: "Interview updated",
+      user: "Angela Mode",
+      date: new Date().toISOString(),
+      meta: {
+        interviewId: interview.id
+      }
+    })
+
+    delete req.session.data.interview
+
+    req.flash('success', 'interview changed')
+    res.redirect(`/application/${applicationId}/interviews`)
+
+  })
+
+
 }
