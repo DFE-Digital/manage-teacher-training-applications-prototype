f4429e07bca60c59b2638b61cd9931915e597f70
Adam Silver <adambsilver@gmail.com>
2020-10-09 14:40:17 +0100

    A few tweaks

---
+++ app/filters/arrays.js
@@ -0,0 +1,194 @@
+// -------------------------------------------------------------------
+// Imports and setup
+// -------------------------------------------------------------------
+var _ = require('lodash');
+// Leave this filters line
+var filters = {}
+
+
+// Merge arrays or strings together in to an array
+filters.combineArrays = (arr1=[], arr2=[]) => {
+  if (_.isString(arr1)) arr1 = [arr1]
+  if (_.isString(arr2)) arr2 = [arr2]
+  return [...arr1, ...arr2]
+}
+
+
+filters.isArray = arr => {
+  let isArray = _.isArray(arr)
+  return _.isArray(arr)
+}
+
+
+/*
+  ====================================================================
+  push
+  --------------------------------------------------------------------
+  push an item in to an array. Returns the array
+  ====================================================================
+
+  Usage:
+
+  {% set array = ['a','b','c'] | push('d') %}
+
+  array == ['a','b','c','d']
+
+*/
+
+filters.push = (array, item) => {
+  array.push(item)
+  return array
+}
+
+filters.trimEach = input => {
+  if (!input) return
+
+  if (_.isArray(input)) return input.map( item => {
+    if (_.isString(item)) return item.trim()
+    else return item
+  } )
+  else if (_.isString(input)) return input.trim()
+  else return input
+}
+
+
+// Remove empty items from arrays / coerce empty to false
+// Returns false if no items remaining
+filters.removeEmpty = items => {
+
+  // Handle empty
+  if (!items) return
+
+  // Handle strings
+  if (_.isString(items) ) {
+    if (items != null && items !== '') return items
+    else return
+  }
+
+  // Handle arrys
+  if (_.isArray(items)){
+    var output = items.filter( item => {
+      return (item && (item !==""))
+    })
+    // Don't return emtpy arrays
+    if (output.length) return output
+    else return
+  }
+}
+
+
+// Remove empty, false and null items from array
+filters.cleanArray = (array) => {
+  let newArray = filters.trimEach(array)
+  newArray = filters.removeEmpty(newArray)
+  return newArray
+}
+
+const joinArray = (array, options={}) => {
+
+  var defaults = {
+    delimiter: ', ',
+    append: '',
+    prepend: ''
+  }
+
+  // Duck-type check for direct Nunjucks macro returns
+
+  // Macro object string
+  if (typeof array == 'object' && 'toString' in array && !_.isArray(array)) {
+    console.log('Error in joinArray: not a string or array', array)
+  }
+
+  // Array contains object string
+  if(_.isArray(array)){
+    array.forEach (item => {
+      if (typeof item == 'object' && 'toString' in item) {
+        console.log('Error in joinArray: not a string or array', array)
+      }
+    })
+  }
+
+  // Deal with strings
+  if (_.isString(array)) {
+    array = [array]
+  }
+  if (_.isString(options)) options = {delimiter: options}
+
+  // Merge options and defaults
+  options = Object.assign({}, defaults, options)
+  // Set to be options.delimiter if it doesn't already exist
+  options.lastDelimiter = options.lastDelimiter || options.delimiter
+
+  // Strip trailing space from delimiters and add breaks
+  if (options.newlines){
+    options.delimiter = options.delimiter.replace(/\s+$/g, '') + "\n"
+    options.lastDelimiter = options.lastDelimiter.replace(/\s+$/g, '') + "\n"
+  }
+
+  // console.log('Input array is', array)
+  // Clean up array
+  array = filters.cleanArray(array)
+  if (!_.isArray(array)) return
+
+  // Don't output anything if no array or items
+  if (!array || array.length == 0) return // return nothing if no items
+
+  // No delimiters if only one item
+  if (array.length == 1) {
+    // console.log(array[0])
+    return options.prepend + array[0] + options.append // just return item
+  }
+
+  // Create string
+  // console.log(array)
+  var last = array.pop();
+  return options.prepend + array.join(options.delimiter) + options.lastDelimiter + last + options.append;
+}
+
+filters.joinArray = joinArray
+
+// A, B, C and D
+filters.joinify = (items) => filters.joinArray(items, {delimiter:', ', lastDelimiter: ' and '})
+
+// A, B, C, and D
+filters.oxfordComma = (items) => filters.joinArray(items, {delimiter:', ', lastDelimiter: ', and '})
+
+//  A, B, C, D
+filters.commaSeparate = (items) => filters.joinArray(items, {delimiter:', '})
+
+//  A,
+//  B,
+//  C,
+//  D
+filters.commaSeparateLines = (items) => filters.joinArray(items, {delimiter:', ', newlines: true })
+
+filters.separateLines = (items) => filters.joinArray(items, {delimiter:' ', newlines: true })
+
+// A and B and C and D
+filters.andSeparate = (items)=> filters.joinArray(items, {delimiter: ' and '})
+
+// A B C D
+filters.spaceSeparate = (items) => filters.joinArray(items, {delimiter:' '})
+
+// A. B. C. D.
+filters.joinAsSentences = (items) => filters.joinArray(items, {delimiter:'. ', append: '.'})
+
+
+filters.removeArrayItem = (array, itemToRemove) =>{
+  if (_.isArray(array)){
+    return filteredItems = array.filter(item => item != itemToRemove)
+  }
+  else return array
+}
+
+filters.removeArrayItems = (array, itemsToRemove) =>{
+  if (_.isArray(array)){
+    return filteredItems = array.filter(item => !itemsToRemove.includes(item))
+  }
+  else return array
+}
+
+// -------------------------------------------------------------------
+// keep the following line to return your filters to the app
+// -------------------------------------------------------------------
+exports.filters = filters
--- app/routes/settings.js
+++ app/routes/settings.js
@@ -1,28 +1,12 @@
 const applications = require('../data/applications')
-
+const _ = require('lodash');
 module.exports = router => {
-  router.post('/admin/settings', (req, res) => {
-    if (Array.isArray(req.body.settings) && req.body.settings.includes('Turn on start new cycle')) {
 
-      let deferredPast = applications
-        .filter(app => app.cycle == "Previous cycle (2019 to 2020)")
-        .filter(app => (app.status == 'Deferred'))
+  router.post('/admin/settings/new-cycle', (req, res) => {
 
-      let acceptedPast = applications
-        .filter(app => app.cycle == "Previous cycle (2019 to 2020)")
-        .filter(app => (app.status == 'Accepted'))
+    var settings = req.session.data.settings
 
-      let other = applications
-        .filter(app => app.status !== 'Awaiting decision')
-        .filter(app => app.status !== 'Deferred')
-        .filter(app => app.status !== 'Offered')
-        .filter(app => app.status !== 'Accepted')
-        .filter(app => app.status !== 'Conditions met')
-        // .filter(app => app.status !== 'Rejected')
-
-      req.session.data.applications = deferredPast.concat(acceptedPast).concat(other);
-
-    } else {
+    if(settings.includes('new-cycle')) {
       req.session.data.applications = applications.filter(app => {
         if (app.status === 'Deferred' && app.cycle === 'Previous cycle (2019 to 2020)') {
           return false;
@@ -37,8 +21,39 @@ module.exports = router => {
           return true;
         }
       })
-    }
+      _.remove(settings, function(item) { return item == 'new-cycle' })
+    } else {
+      let deferredPast = applications
+        .filter(app => app.cycle == "Previous cycle (2019 to 2020)")
+        .filter(app => (app.status == 'Deferred'))
 
-    res.redirect('/')
+      let acceptedPast = applications
+        .filter(app => app.cycle == "Previous cycle (2019 to 2020)")
+        .filter(app => (app.status == 'Accepted'))
+
+      let other = applications
+        .filter(app => app.status !== 'Awaiting decision')
+        .filter(app => app.status !== 'Deferred')
+        .filter(app => app.status !== 'Offered')
+        .filter(app => app.status !== 'Accepted')
+        .filter(app => app.status !== 'Conditions met')
+
+      req.session.data.applications = deferredPast.concat(acceptedPast).concat(other);
+
+      settings.push('new-cycle')
+    }
+    res.redirect('/admin/settings')
   })
+
+  router.post('/admin/settings/combine-conditions', (req, res) => {
+    var settings = req.session.data.settings
+
+    if(settings.includes('hasCombinedConditions')) {
+      _.remove(settings, function(item) { return item == 'hasCombinedConditions' })
+    } else {
+       settings.push('hasCombinedConditions')
+    }
+    res.redirect('/admin/settings')
+  })
+
 }
