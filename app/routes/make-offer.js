e8c0a4b6b0f764f820083a50d21fd875f052e265
Adam Silver <adambsilver@gmail.com>
2020-10-16 10:43:31 +0100

    New offer multiple conditions

--- app/routes/make-offer.js
+++ app/routes/make-offer.js
@@ -1,82 +1,76 @@
 const { v4: uuidv4 } = require('uuid')
 
 module.exports = router => {
+
   router.get('/application/:applicationId/offer/new', (req, res) => {
-    res.render('offer/new/standard/conditions', {
-      application: req.session.data.applications.find(app => app.id === req.params.applicationId)
+    let application = req.session.data.applications.find(app => app.id === req.params.applicationId)
+    let conditions;
+
+    // cleanse data gah
+    if(req.session.data['new-offer'] && req.session.data['new-offer']['conditions']) {
+      req.session.data['new-offer']['conditions'] = req.session.data['new-offer']['conditions'].filter(c => c != '')
+    }
+
+    conditions = req.session.data['new-offer']['conditions']
+
+    res.render('application/offer/new/index', {
+      application,
+      conditions
     })
   })
 
   router.post('/application/:applicationId/offer/new', (req, res) => {
     const applicationId = req.params.applicationId
-    res.redirect(`/application/${applicationId}/offer/new/confirm`)
+    res.redirect(`/application/${applicationId}/offer/new/check`)
   })
 
-  // Show confirmation
-  router.get('/application/:applicationId/offer/new/confirm', (req, res) => {
-    const data = req.session.data;
-    if (req.session.data['standard-conditions']) {
-      data.standardConditions = req.session.data['standard-conditions'].map((item) => {
-        return {
-          description: item
-        }
-      })
+  router.get('/application/:applicationId/offer/new/check', (req, res) => {
+    let application = req.session.data.applications.find(app => app.id === req.params.applicationId)
+    var conditions = []
+
+    if(req.session.data['new-offer']['standard-conditions'].length) {
+      conditions = conditions.concat(req.session.data['new-offer']['standard-conditions'])
     }
 
-    data.furtherConditions = []
+    req.session.data['new-offer']['conditions'].filter(c => c != '').forEach(c => {
+      conditions.push(c)
+    })
 
-    if (data['further-conditions']['condition-1']){
-      data.furtherConditions.push({ description: data['further-conditions']['condition-1'] })
-    }
-    if (data['further-conditions']['condition-2']){
-      data.furtherConditions.push({ description: data['further-conditions']['condition-2'] })
-    }
-    if (data['further-conditions']['condition-3']){
-      data.furtherConditions.push({ description: data['further-conditions']['condition-3'] })
-    }
-    if (data['further-conditions']['condition-4']){
-      data.furtherConditions.push({ description: data['further-conditions']['condition-4'] })
-    }
-
-    res.render('offer/new/standard/confirm', {
-      application: req.session.data.applications.find(app => app.id === req.params.applicationId),
-      conditions: data.standardConditions.concat(data.furtherConditions)
+    res.render('application/offer/new/check', {
+      application,
+      conditions
     })
   })
 
-  router.post('/application/:applicationId/offer/new/confirm', (req, res) => {
+  router.post('/application/:applicationId/offer/new/check', (req, res) => {
     const applicationId = req.params.applicationId
     const application = req.session.data.applications.find(app => app.id === applicationId)
-    const data = req.session.data;
+
     application.status = 'Offered'
     application.offer = {
       madeDate: new Date().toISOString()
     }
 
-    application.offer.standardConditions = data.standardConditions.map((item) => {
+    // save standard conditions
+    if(req.session.data['new-offer']['standard-conditions'].length) {
+      application.offer.standardConditions = req.session.data['new-offer']['standard-conditions'].map(c => {
+        return {
+          id: uuidv4(),
+          description: c,
+          status: "Pending"
+        }
+      })
+    }
+
+    // save further conditions
+    application.offer.conditions = req.session.data['new-offer']['conditions'].filter(c => c != '').map(c => {
       return {
         id: uuidv4(),
-        description: item.description,
-        status: 'Pending'
+        description: c,
+        status: "Pending"
       }
     })
 
-    const conditions = []
-
-    data.furtherConditions.forEach(condition => {
-      conditions.push({ id: uuidv4(), description: condition.description, status: 'Pending' })
-    })
-
-    // Delete form field data
-    delete data['standard-conditions']
-    delete data['further-conditions']
-    // Delete transformed data
-    delete data.furtherConditions
-    delete data.standardConditions
-
-    application.offer.conditions = conditions
-
-    application.offer.recommendations = req.session.data.recommendations
     req.flash('success', 'Offer successfully made')
     res.redirect(`/application/${req.params.applicationId}/offer`)
   })
