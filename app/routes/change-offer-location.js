0a7935627dc9a39cb468d09b697899f2f38fdbe3
Adam Silver <adambsilver@gmail.com>
2020-11-03 16:11:05 +0000

    Make urls consistent

--- app/routes/change-offer-location.js
+++ app/routes/change-offer-location.js
@@ -1,23 +1,22 @@
 module.exports = router => {
-  router.get('/application/:applicationId/offer/change-location', (req, res) => {
-    res.render('application/offer/edit-location/location', {
+  router.get('/application/:applicationId/offer/edit-location', (req, res) => {
+    res.render('application/offer/edit-location/index', {
       application: req.session.data.applications.find(app => app.id === req.params.applicationId)
     })
   })
 
-  router.post('/application/:applicationId/offer/change-location', (req, res) => {
-    res.redirect(`/application/${req.params.applicationId}/offer/change-location/confirm`)
+  router.post('/application/:applicationId/offer/edit-location', (req, res) => {
+    res.redirect(`/application/${req.params.applicationId}/offer/edit-location/check`)
   })
 
-  router.get('/application/:applicationId/offer/change-location/confirm', (req, res) => {
-    res.render('application/offer/edit-location/confirm', {
+  router.get('/application/:applicationId/offer/edit-location/check', (req, res) => {
+    res.render('application/offer/edit-location/check', {
       application: req.session.data.applications.find(app => app.id === req.params.applicationId)
     })
   })
 
-  router.post('/application/:applicationId/offer/change-location/confirm', (req, res) => {
+  router.post('/application/:applicationId/offer/edit-location/check', (req, res) => {
     const applicationId = req.params.applicationId
-    // const application = req.session.data.applications.find(app => app.id == applicationId)
     req.flash('success', 'Offer successfully changed')
     res.redirect(`/application/${applicationId}/offer`)
   })
