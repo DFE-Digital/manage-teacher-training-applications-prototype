95fd8a033abfa02ea78bd96eecd7ab561d53dc20
Simon Whatley <23444+simonwhatley@users.noreply.github.com>
2020-11-12 11:45:35 +0000

    Remove MOJ frontend dependency (#240)

--- app/assets/javascripts/modules/filter-toggle-button.js
+++ app/assets/javascripts/modules/filter-toggle-button.js
@@ -1,4 +1,4 @@
-MOJFrontend.FilterToggleButton = function(options) {
+AppFrontend.FilterToggleButton = function(options) {
   this.options = options;
   this.container = this.options.toggleButton.container;
   this.createToggleButton();
@@ -9,19 +9,19 @@ MOJFrontend.FilterToggleButton = function(options) {
   }
 };
 
-MOJFrontend.FilterToggleButton.prototype.setupResponsiveChecks = function() {
+AppFrontend.FilterToggleButton.prototype.setupResponsiveChecks = function() {
   this.mq = window.matchMedia(this.options.bigModeMediaQuery);
   this.mq.addListener($.proxy(this, 'checkMode'));
   this.checkMode(this.mq);
 };
 
-MOJFrontend.FilterToggleButton.prototype.createToggleButton = function() {
+AppFrontend.FilterToggleButton.prototype.createToggleButton = function() {
   this.menuButton = $('<button class="govuk-button '+this.options.toggleButton.classes+'" type="button" aria-haspopup="true" aria-expanded="false">'+this.options.toggleButton.showText+'</button>');
   this.menuButton.on('click', $.proxy(this, 'onMenuButtonClick'));
   this.options.toggleButton.container.append(this.menuButton);
 };
 
-MOJFrontend.FilterToggleButton.prototype.checkMode = function(mq) {
+AppFrontend.FilterToggleButton.prototype.checkMode = function(mq) {
   if(mq.matches) {
     this.enableBigMode();
   } else {
@@ -29,53 +29,53 @@ MOJFrontend.FilterToggleButton.prototype.checkMode = function(mq) {
   }
 };
 
-MOJFrontend.FilterToggleButton.prototype.enableBigMode = function() {
+AppFrontend.FilterToggleButton.prototype.enableBigMode = function() {
   this.showMenu();
   this.removeCloseButton();
 };
 
-MOJFrontend.FilterToggleButton.prototype.enableSmallMode = function() {
+AppFrontend.FilterToggleButton.prototype.enableSmallMode = function() {
   this.hideMenu();
   this.addCloseButton();
 };
 
-MOJFrontend.FilterToggleButton.prototype.addCloseButton = function() {
+AppFrontend.FilterToggleButton.prototype.addCloseButton = function() {
   if(this.options.closeButton) {
-    this.closeButton = $('<button class="moj-filter__close" type="button">'+this.options.closeButton.text+'</button>');
+    this.closeButton = $('<button class="app-filter__close" type="button">'+this.options.closeButton.text+'</button>');
     this.closeButton.on('click', $.proxy(this, 'onCloseClick'));
     this.options.closeButton.container.append(this.closeButton);
   }
 };
 
-MOJFrontend.FilterToggleButton.prototype.onCloseClick = function() {
+AppFrontend.FilterToggleButton.prototype.onCloseClick = function() {
   this.hideMenu();
   this.menuButton.focus();
 };
 
-MOJFrontend.FilterToggleButton.prototype.removeCloseButton = function() {
+AppFrontend.FilterToggleButton.prototype.removeCloseButton = function() {
   if(this.closeButton) {
     this.closeButton.remove();
     this.closeButton = null;
   }
 };
 
-MOJFrontend.FilterToggleButton.prototype.hideMenu = function() {
+AppFrontend.FilterToggleButton.prototype.hideMenu = function() {
   this.menuButton.attr('aria-expanded', 'false');
-  this.options.filter.container.addClass('moj-js-hidden');
+  this.options.filter.container.addClass('app-js-hidden');
   this.menuButton.text(this.options.toggleButton.showText);
 };
 
-MOJFrontend.FilterToggleButton.prototype.showMenu = function() {
+AppFrontend.FilterToggleButton.prototype.showMenu = function() {
   this.menuButton.attr('aria-expanded', 'true');
-  this.options.filter.container.removeClass('moj-js-hidden');
+  this.options.filter.container.removeClass('app-js-hidden');
   this.menuButton.text(this.options.toggleButton.hideText);
 };
 
-MOJFrontend.FilterToggleButton.prototype.onMenuButtonClick = function() {
+AppFrontend.FilterToggleButton.prototype.onMenuButtonClick = function() {
   this.toggle();
 };
 
-MOJFrontend.FilterToggleButton.prototype.toggle = function() {
+AppFrontend.FilterToggleButton.prototype.toggle = function() {
   if(this.menuButton.attr('aria-expanded') == 'false') {
     this.showMenu();
     this.options.filter.container.focus();
