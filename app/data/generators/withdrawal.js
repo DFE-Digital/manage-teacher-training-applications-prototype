b2262887752cfab344e9f7b61a3c19bf8531741e
John Oates <mail@johnoates.co.uk>
2020-11-24 16:54:33 +0000

    Further changes

--- app/data/generators/withdrawal.js
+++ app/data/generators/withdrawal.js
@@ -1,27 +1,27 @@
 module.exports = (faker) => ({
   // Candidate actions
   actions: 'Yes',
-  'actions-reasons': 'Did not reply to interview request, Did not attend interview',
-  'actions-reasons-other': 'Another reasons goes here',
+  'actions-reasons': ['Did not reply to interview request', 'Did not attend interview'],
+  'actions-reasons-other': 'Another reason goes here',
 
   // Course full
   'course-full': 'Yes',
 
   // Missing qualifications
   'missing-qualifications': 'Yes',
-  'missing-qualifications-reasons': 'No maths GCSE grade 4 (C) or above, or accepted equivalent,No English GCSE grade 4 (C) or above, or accepted equivalent, or valid equivalent, Other',
+  'missing-qualifications-reasons': ['No maths GCSE grade 4 (C) or above, or accepted equivalent','No English GCSE grade 4 (C) or above, or accepted equivalent', 'Other'],
   'missing-qualifications-reasons-other': 'Another reason goes here',
 
   // Application quality
   'application-quality': 'Yes',
-  'application-quality-reasons': 'Personal statement could be improved, Subject knowledge could be improved, Other',
+  'application-quality-reasons': ['Personal statement could be improved', 'Subject knowledge could be improved', 'Other'],
   'application-quality-reasons-other': 'Spelling wasn’t great',
   'application-quality-reasons-subject-knowledge': "Didn't know enough about maths",
   'application-quality-reasons-personal-statement': 'Lack of passion',
 
   // Safeguarding
   safeguarding: 'Yes',
-  'safeguarding-reasons': 'Inaccurate or false information in the application,Evidence of plagiarism in the application, References did not support the application, The candidate disclosed information which makes them unsuitable to work with children, The vetting process found information which makes the candidate unsuitable to work with children',
+  'safeguarding-reasons': ['Inaccurate or false information in the application','Evidence of plagiarism in the application', 'References did not support the application', 'The candidate disclosed information which makes them unsuitable to work with children', 'The vetting process found information which makes the candidate unsuitable to work with children'],
   'safeguarding-reasons-false-information': 'False information about past',
   'safeguarding-reasons-plagiarism': 'Copied a blog post',
   'safeguarding-reasons-reference-information': 'John had a patchy history',
