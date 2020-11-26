f28b673b4e7165125f32ac8e979af1b47fb01b46
Simon Whatley <23444+simonwhatley@users.noreply.github.com>
2020-11-26 09:58:59 +0000

    Update activity log (#259)

--- app/routes/activity-log.js
+++ app/routes/activity-log.js
@@ -1,3 +1,77 @@
+const _ = require("lodash")
+const { DateTime } = require('luxon')
+
+function getActivity(applications) {
+  let activity = []
+
+  applications.forEach(app => {
+    const events = app.events.items.map(item => {
+      return {
+        app: app,
+        event: item
+      }
+    })
+    activity = activity.concat(events)
+  })
+
+  activity.sort((a, b) => {
+    return new Date(b.event.date) - new Date(a.event.date)
+  })
+
+  return activity
+}
+
+function groupByDate(data) {
+  return _.groupBy(data, (item) => {
+    const itemDate = DateTime.fromISO(item.event.date)
+    const groupDate = DateTime.fromObject({
+      day: itemDate.day,
+      month: itemDate.month,
+      year: itemDate.year,
+    })
+    return groupDate.toString()
+  })
+}
+
+function getDataByPage(data, pageNumber, pageSize = 50) {
+  --pageNumber // because pages logically start with 1, but technically with 0
+  return data.slice(pageNumber * pageSize, (pageNumber + 1) * pageSize)
+}
+
+function getPaginationItems(page, pageCount) {
+  let startItem = 1;
+  let endItem = 5;
+
+  // First five pages
+  if (page > 3) {
+    startItem = page - 2;
+    endItem = page + 2;
+  }
+
+  // Last five pages
+  if (page > (pageCount - 3)) {
+    startItem = pageCount - 4;
+    endItem = pageCount;
+  }
+
+  // items: [{
+  //   text: '1',
+  //   href: '?page=1',
+  //   selected: true
+  // }]
+
+  const itemArray = []
+  for (let i = startItem; i <= endItem; i++) {
+    let item = {}
+    item.text = i
+    item.href = '?page=' + i
+    item.selected = true ? parseInt(page) === i : false
+    itemArray.push(item)
+  }
+
+  return itemArray
+}
+
 module.exports = router => {
   router.get('/activity', (req, res) => {
     // Clone and turn into an array
@@ -5,34 +79,44 @@ module.exports = router => {
       return app.cycle === "2020 to 2021"
     })
 
-    let allEvents = []
+    // Get the activity
+    let activity = getActivity(apps)
 
-    apps.forEach(app => {
-      const events = app.events.items.map(item => {
-        return {
-          app: app,
-          event: item
-        }
-      })
-      allEvents = allEvents.concat(events)
+    // Total number of activity events
+    let count = activity.length;
+
+    // Prevent users putting in a limit not in the pre-defined set: 10, 25, 50, 100
+    let limit = 50;
+    if ([10,25,50,100].indexOf(parseInt(req.query.limit)) !== -1) {
+      limit = (req.query.limit) ? parseInt(req.query.limit) : 50
+    }
+
+    // Current page
+    let page = (req.query.page) ? parseInt(req.query.page) : 1
+
+    // Total number of pages
+    let pageCount = Math.ceil(count / limit)
+
+    // Calculate the previous and next pages
+    let prevPage = (page - 1) ? (page - 1) : 1
+    let nextPage = ((page + 1) > pageCount) ? pageCount : (page + 1)
+
+    let startItem = (page == 1) ? page : ((page * limit) - limit) + 1;
+    let endItem = (page == 1) ? (page * limit) : ((startItem + limit) - 1);
+
+    activity = getDataByPage(activity, page, limit)
+
+    activity = groupByDate(activity)
+
+    res.render('activity/index', {
+      activity: activity,
+      totalCount: count,
+      pageSize: limit,
+      pageCount: pageCount,
+      pageNumber: page,
+      pageItems: getPaginationItems(page, pageCount),
+      startItem: startItem,
+      endItem: endItem
     })
-
-    allEvents.sort((a, b) => {
-      return new Date(b.event.date) - new Date(a.event.date)
-    })
-
-    const todayEvents = allEvents.filter(event => {
-      return event.event.date.indexOf('2020-08-12') >= 0
-    })
-
-    const yesterdayEvents = allEvents.filter(event => {
-      return event.event.date.indexOf('2020-08-11') >= 0
-    })
-
-    const allOtherEvents = allEvents.filter(event => {
-      return event.event.date.indexOf('2020-08-11') === -1 && event.event.date.indexOf('2020-08-12') === -1
-    })
-
-    res.render('activity/index', { todayEvents, yesterdayEvents, allOtherEvents })
   })
 }
