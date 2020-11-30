2bf6fa15f31de3569c5279049ae74d2413c03fdf
Simon Whatley <simonwhatley@gmail.com>
2020-11-30 15:54:35 +0000

    Fix status generator

--- app/data/generators/status.js
+++ app/data/generators/status.js
@@ -1,12 +1,16 @@
 module.exports = (faker) => {
   return faker.helpers.randomize([
-    'Deferred',
-    'Awaiting decision',
     'Accepted',
-    'Declined',
-    'Offer withdrawn',
-    'Rejected',
     'Application withdrawn',
-    'Conditions not met'
+    'Awaiting decision',
+    'Conditions not',
+    'Conditions not met',
+    'Declined',
+    'Deferred',
+    'Interviewing',
+    'Offered',
+    'Offer withdrawn',
+    'Received',
+    'Rejected'
   ])
 }
