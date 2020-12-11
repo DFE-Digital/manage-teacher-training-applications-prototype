d5e4bed1be7f2a3ca4788dbcd4eb1c76ebdfbd1d
Adam Silver <adambsilver@gmail.com>
2020-12-11 16:00:51 +0000

    Persist answer

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -67,6 +67,7 @@ let relationships = [{
 let userOrgs = [trainingProviders[0], accreditedBodies[0], accreditedBodies[1]];
 
 module.exports = {
+  emailsettings: ['Receive email notifications'],
   settings: [],
   user: {
     givenName: "Claudine",
