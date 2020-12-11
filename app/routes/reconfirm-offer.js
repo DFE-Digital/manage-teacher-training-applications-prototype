66374d072ae02dfd93955bdd4921160d60cc46c3
Simon Whatley <23444+simonwhatley@users.noreply.github.com>
2020-12-11 09:28:24 +0000

    Fix offer reconfirmation (#278)

--- app/routes/reconfirm-offer.js
+++ app/routes/reconfirm-offer.js
@@ -75,7 +75,7 @@ module.exports = router => {
 
     application.offer.madeDate = new Date().toISOString()
     application.status = 'Accepted' // work this out
-    application.cycle = 'Current cycle (2020 to 2021)'
+    application.cycle = '2020 to 2021'
     application.events.items.push({
       date: new Date().toISOString(),
       user: "Alicia Grenada",
