e0a85a6f6d2ab8ff6361b050d409dcafed83b355
Adam Silver <adambsilver@gmail.com>
2020-12-14 09:08:38 +0000

    Persist answer

--- app/routes/notifications.js
+++ app/routes/notifications.js
@@ -1,6 +1,9 @@
 
 module.exports = router => {
   router.post('/account/notifications', (req, res) => {
+
+    req.session.data.emailsettings = req.body.emailsettings
+
     req.flash('success', 'Email settings saved')
     res.redirect("/account/notifications")
   })
