4fd7777c5eebfbedae25c0a2b8b4d86bbab0caa9
Adam Silver <adambsilver@gmail.com>
2021-01-13 07:54:27 +0000

    Separate flow for withdrawing an application

---
+++ app/routes/withdraw-application.js
@@ -0,0 +1,39 @@
+const ApplicationHelper = require('../data/helpers/application')
+
+module.exports = router => {
+  router.get('/applications/:applicationId/withdraw', (req, res) => {
+    res.render('applications/withdraw/index', {
+      application: req.session.data.applications.find(app => app.id === req.params.applicationId)
+    })
+  })
+
+  router.post('/applications/:applicationId/withdraw', (req, res) => {
+    res.redirect(`/applications/${req.params.applicationId}/withdraw/other-reasons-for-rejection`)
+  })
+
+  router.get('/applications/:applicationId/withdraw/other-reasons-for-rejection', (req, res) => {
+    res.render('applications/withdraw/other-reasons-for-rejection', {
+      application: req.session.data.applications.find(app => app.id === req.params.applicationId)
+    })
+  })
+
+  router.post('/applications/:applicationId/withdraw/other-reasons-for-rejection', (req, res) => {
+    res.redirect(`/applications/${req.params.applicationId}/withdraw/check`)
+  })
+
+  router.get('/applications/:applicationId/withdraw/check', (req, res) => {
+    res.render('applications/withdraw/check', {
+      application: req.session.data.applications.find(app => app.id === req.params.applicationId)
+    })
+  })
+
+  router.post('/applications/:applicationId/withdraw/check', (req, res) => {
+    const applicationId = req.params.applicationId
+    const application = req.session.data.applications.find(app => app.id === applicationId)
+    application.status = 'Application withdrawn'
+    application.withdrawnDate = new Date().toISOString()
+    req.flash('success', 'Application closed')
+    delete req.session.data.rejectionReasons
+    res.redirect(`/applications/${applicationId}`)
+  })
+}
