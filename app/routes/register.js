5e6b00406fe8d3d5966ede4cbf1924b596026449
Simon Whatley <simonwhatley@gmail.com>
2021-01-16 21:06:22 +0000

    Update routing

--- app/routes/register.js
+++ app/routes/register.js
@@ -9,21 +9,26 @@ function checkHasAnswers (req, res, next) {
   }
 }
 
+function getTrainingProvidersIds (data) {
+  const array = []
+  for (let i = 0; i < data.length; i++) {
+    array.push(data[i].id)
+  }
+  return array
+}
+
 module.exports = router => {
 
   router.get('/register', (req, res) => {
-
     // delete any previous onboarding data
     delete req.session.data.registration
 
     res.render('register/index', {
       organisations
     })
-
   })
 
   router.get('/register/:organisationId/start', (req, res) => {
-
     // set up the structure into which we'll put the onboarding data
     req.session.data.registration = {}
 
@@ -38,6 +43,9 @@ module.exports = router => {
 
     // put the training prviders into the session for convenience
     req.session.data.registration.trainingProviders = trainingProviders
+
+    // put the training provider ids into an array so we can use them to work out back routing
+    req.session.data.registration.trainingProvidersIds = getTrainingProvidersIds(trainingProviders)
 
     // get the number of training providers. We'll use this to iterate through the list of providers
     req.session.data.registration.trainingProviderCount = trainingProviders.length
@@ -55,20 +63,26 @@ module.exports = router => {
       accreditingBody,
       trainingProviders
     })
-
   })
 
 
   router.get('/register/:organisationId/providers/:providerId', checkHasAnswers, (req, res) => {
-
     const trainingProvider = req.session.data.registration.trainingProviders.filter(org => org.id === req.params.providerId)[0]
 
-    // TODO: fix back link to go to correct provider page (use array position?)
     // TODO: fix back button for check your answers referrer
+    // set the back button default to the start page
     let back = `/register/${req.params.organisationId}/start`
+    // if we're no on the first provider, we need to change the back button
     if (req.session.data.registration.trainingProviderPosition > 0) {
-      const previousProviderId = req.session.data.registration.previousProviderId
-      back = `/register/${req.params.organisationId}/providers/${previousProviderId}`
+      // get the position of the current provider id
+      const position = req.session.data.registration.trainingProvidersIds.indexOf(req.params.providerId)
+      // if the current position isn't the first item
+      if (position) {
+        // get the previous provider id from the array
+        const previousProviderId = req.session.data.registration.trainingProvidersIds[position-1]
+        // set the back link
+        back = `/register/${req.params.organisationId}/providers/${previousProviderId}`
+      }
     }
 
     res.render('register/provider', {
@@ -78,10 +92,10 @@ module.exports = router => {
       },
       trainingProvider
     })
-
   })
 
   router.post('/register/:organisationId/providers/:providerId', checkHasAnswers, (req, res) => {
+    // TODO: fix session data storage
 
     // combine the provider form details with the associated training provider object
     req.session.data.registration.trainingProviders[req.session.data.registration.trainingProviderPosition] = {...req.session.data.registration.trainingProviders[req.session.data.registration.trainingProviderPosition], ...req.session.data.registration.onboarding}
@@ -91,7 +105,6 @@ module.exports = router => {
 
     // if we've reached the last provider, move to the next step, else next continue with the providers
     if (req.session.data.registration.trainingProviderPosition == (req.session.data.registration.trainingProviderCount-1)) {
-
       // set the last provider id for use in the back link
       req.session.data.registration.lastProviderId = req.params.providerId
 
@@ -99,7 +112,6 @@ module.exports = router => {
       res.redirect(`/register/${req.params.organisationId}/agreement`)
 
     } else {
-
       // set the previous provider id for use in the back link
       req.session.data.registration.previousProviderId = req.params.providerId
 
@@ -111,11 +123,9 @@ module.exports = router => {
 
       res.redirect(`/register/${req.params.organisationId}/providers/${nextTrainingProviderId}`)
     }
-
   })
 
   router.get('/register/:organisationId/agreement', checkHasAnswers, (req, res) => {
-
     const lastProviderId = req.session.data.registration.lastProviderId
 
     res.render('register/agreement', {
@@ -129,13 +139,10 @@ module.exports = router => {
   })
 
   router.post('/register/:organisationId/agreement', checkHasAnswers, (req, res) => {
-
     res.redirect(`/register/${req.params.organisationId}/check-your-answers`)
-
   })
 
   router.get('/register/:organisationId/check-your-answers', checkHasAnswers, (req, res) => {
-
     // set invitation count for use in showing content
     const trainingProviderInviteCount = req.session.data.registration.trainingProviders.filter(org => org.onboard == 'yes').length
 
@@ -147,18 +154,15 @@ module.exports = router => {
       registration: req.session.data.registration,
       trainingProviderInviteCount
     })
-
   })
 
   router.get('/register/:organisationId/done', checkHasAnswers, (req, res) => {
-
     // set invitation count for use in pluralising content
     const trainingProviderInviteCount = req.session.data.registration.trainingProviders.filter(org => org.onboard == 'yes').length
 
     res.render('register/done', {
       trainingProviderInviteCount
     })
-
   })
 
 }
