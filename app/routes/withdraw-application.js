de04da4f4419d9780d7f65c6ec4662344134080e
Adam Silver <adambsilver@gmail.com>
2021-04-14 13:43:21 +0100

    Tidy withdraw application flow

--- app/routes/withdraw-application.js
+++ app/routes/withdraw-application.js
@@ -2,26 +2,6 @@ const ApplicationHelper = require('../data/helpers/application')
 
 module.exports = router => {
   router.get('/applications/:applicationId/withdraw', (req, res) => {
-    res.render('applications/withdraw/index', {
-      application: req.session.data.applications.find(app => app.id === req.params.applicationId)
-    })
-  })
-
-  router.post('/applications/:applicationId/withdraw', (req, res) => {
-    res.redirect(`/applications/${req.params.applicationId}/withdraw/check`)
-  })
-
-  // router.get('/applications/:applicationId/withdraw/other-advice', (req, res) => {
-  //   res.render('applications/withdraw/other-advice', {
-  //     application: req.session.data.applications.find(app => app.id === req.params.applicationId)
-  //   })
-  // })
-
-  // router.post('/applications/:applicationId/withdraw/other-advice', (req, res) => {
-  //   res.redirect(`/applications/${req.params.applicationId}/withdraw/check`)
-  // })
-
-  router.get('/applications/:applicationId/withdraw/check', (req, res) => {
     const applicationId = req.params.applicationId
     const application = req.session.data.applications.find(app => app.id === applicationId)
     res.render('applications/withdraw/check', {
@@ -30,12 +10,11 @@ module.exports = router => {
     })
   })
 
-  router.post('/applications/:applicationId/withdraw/check', (req, res) => {
+  router.post('/applications/:applicationId/withdraw', (req, res) => {
     const applicationId = req.params.applicationId
     const application = req.session.data.applications.find(app => app.id === applicationId)
     application.status = 'Application withdrawn'
     application.withdrawalDate = new Date().toISOString()
-    application.withdrawalReasons = ApplicationHelper.getApplicationWithdrawnReasons(req.session.data.withdraw)
     req.flash('success', 'Application withdrawn')
     delete req.session.data.withdraw
 
@@ -48,6 +27,6 @@ module.exports = router => {
       "user": "Ben Brown",
       "date": new Date().toISOString()
     })
-    res.redirect(`/applications/${applicationId}/feedback`)
+    res.redirect(`/applications/${applicationId}`)
   })
 }
