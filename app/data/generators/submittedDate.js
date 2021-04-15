f658c7b03f9ccc7e44d650d99341919e8ef83467
Adam Silver <adambsilver@gmail.com>
2021-04-15 09:29:42 +0100

    Improve dates

--- app/data/generators/submittedDate.js
+++ app/data/generators/submittedDate.js
@@ -5,12 +5,43 @@ faker.locale = 'en_GB'
 module.exports = (params) => {
   let submittedDate
 
+
   if(params.status == "Offered") {
     submittedDate = SystemHelper.now().minus({
-      days: faker.random.number({ 'min': 10, 'max': 60 })
+      days: faker.random.number({ 'min': 60, 'max': 70 })
+    })
+  } else if(params.status == "Offer withdrawn") {
+    submittedDate = SystemHelper.now().minus({
+      days: faker.random.number({ 'min': 60, 'max': 180 })
+    })
+  } else if(params.status == "Rejected") {
+    submittedDate = SystemHelper.now().minus({
+      days: faker.random.number({ 'min': 60, 'max': 180 })
+    })
+  } else if(params.status == "Ready to enroll") {
+    submittedDate = SystemHelper.now().minus({
+      days: faker.random.number({ 'min': 60, 'max': 180 })
+    })
+  } else if(params.status == "Conditions not met") {
+    submittedDate = SystemHelper.now().minus({
+      days: faker.random.number({ 'min': 20, 'max': 180 })
+    })
+  } else if(params.status == "Offer declined") {
+    submittedDate = SystemHelper.now().minus({
+      days: faker.random.number({ 'min': 60, 'max': 180 })
+    })
+  } else if(params.status == "Application withdrawn") {
+    submittedDate = SystemHelper.now().minus({
+      days: faker.random.number({ 'min': 60, 'max': 180 })
+    })
+  } else if(params.status == "Offer accepted") {
+    submittedDate = SystemHelper.now().minus({
+      days: faker.random.number({ 'min': 60, 'max': 180 })
     })
   } else {
-    submittedDate = SystemHelper.now().minus({ days: 20 }).toISO()
+    submittedDate = SystemHelper.now().minus({
+      days: faker.random.number({ 'min': 0, 'max': 40 })
+    }).toISO()
   }
 
   return submittedDate
