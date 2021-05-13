ab5087d32450e75c4a6fe7a0e1bc3278202b3ad9
Adam Silver <adambsilver@gmail.com>
2021-05-13 15:52:10 +0100

    Fix org perms

--- app/routes/organisational-permissions.js
+++ app/routes/organisational-permissions.js
@@ -1,48 +1,42 @@
 const ApplicationHelper = require('../data/helpers/application')
 
-function getUserRelationships(params) {
-  return params.userOrgs.map(org => {
+function getRelationships(params) {
+  let relationships = []
 
-    var item = { org: org, relationships: [] };
-    params.orgRelationships
-      .filter(relationship => {
-        return relationship.org1.id == org.id || relationship.org2.id == org.id
-      })
-      .forEach(relationship => {
-        if(relationship.org1.id == org.id) {
-          item.relationships.push({
-            id: relationship.id,
-            org: relationship.org1,
-            orgPermissions: relationship.org1Permissions,
-            partner: relationship.org2,
-            partnerPermissions: relationship.org2Permissions
-          })
-        } else {
-          // swap it about
-          item.relationships.push({
-            id: relationship.id,
-            org: relationship.org2,
-            orgPermissions: relationship.org2Permissions,
-            partner: relationship.org1,
-            partnerPermissions: relationship.org1Permissions
-          })
-        }
-      })
+  params.relationships
+    .filter(relationship => {
+      return relationship.org1.id == params.org.id || relationship.org2.id == params.org.id
+    })
+    .forEach(relationship => {
+      if(relationship.org1.id == params.org.id) {
+        relationships.push({
+          id: relationship.id,
+          org: relationship.org1,
+          orgPermissions: relationship.org1Permissions,
+          partner: relationship.org2,
+          partnerPermissions: relationship.org2Permissions
+        })
+      } else {
+        // swap it about
+        relationships.push({
+          id: relationship.id,
+          org: relationship.org2,
+          orgPermissions: relationship.org2Permissions,
+          partner: relationship.org1,
+          partnerPermissions: relationship.org1Permissions
+        })
+      }
+    })
 
-    return item
+  return relationships
 
-  })
 }
 
 module.exports = router => {
 
   router.get('/organisation-settings/:orgId/organisational-permissions', (req, res) => {
-    let userOrgs = req.session.data.user.organisations
-    let orgRelationships = req.session.data.relationships
-    let relationships = getUserRelationships({
-      userOrgs,
-      orgRelationships
-    })
+    let org = req.session.data.user.organisations.find(org => req.params.orgId == org.id)
+    let relationships = getRelationships({ org, relationships: req.session.data.relationships })
 
     res.render('organisation-settings/organisational-permissions/index', {
       org: req.session.data.user.organisations.find(org => org.id == req.params.orgId),
