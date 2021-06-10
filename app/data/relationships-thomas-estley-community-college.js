14ae09f49c8de651e62adca95800a3bca5c5b562
Adam Silver <adambsilver@gmail.com>
2021-06-10 14:08:52 +0100

    Add Thomas Estley data

---
+++ app/data/relationships-thomas-estley-community-college.js
@@ -0,0 +1,52 @@
+const OrgHelper = require('./helpers/organisation')
+let relationships = []
+let userOrg = OrgHelper.findOrg("Thomas Estley Community College")
+
+relationships.push({
+  id: 1,
+  org1: userOrg,
+  org1Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  },
+  org2: OrgHelper.findOrg("Leicestershire Secondary SCITT"),
+  org2Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  }
+})
+relationships.push({
+  id: 2,
+  org1: userOrg,
+  org1Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  },
+  org2: OrgHelper.findOrg("University of Leicester"),
+  org2Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  }
+})
+relationships.push({
+  id: 3,
+  org1: userOrg,
+  org1Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  },
+  org2: OrgHelper.findOrg("The University of Warwick"),
+  org2Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  }
+})
+
+
+module.exports = relationships
