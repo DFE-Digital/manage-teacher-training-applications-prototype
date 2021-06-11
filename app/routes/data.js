37676978eceeb64acda3877c10e2422579bdc177
Simon Whatley <simonwhatley@gmail.com>
2021-06-11 15:30:25 +0100

    Update reports for multiple organisations

--- app/routes/data.js
+++ app/routes/data.js
@@ -3,14 +3,7 @@ const StatisticsHelper = require('../data/helpers/statistics')
 module.exports = router => {
 
   router.get('/data', (req, res) => {
-    const userOrganisations = req.session.data.user.organisations.map((organisation) => {
-      return organisation.name
-    })
-
-    // const userOrganisations = [
-    //   'University of Bedfordshire',
-    //   'University of Hertfordshire'
-    // ]
+    const userOrganisations = req.session.data.user.organisations
 
     res.render('data/index', {
       userOrganisations
@@ -33,7 +26,33 @@ module.exports = router => {
     const statusData = StatisticsHelper.statusData
 
     res.render('data/statistics/status', {
-      organisation: req.query.organisation,
+      statuses,
+      statusData,
+      partners
+    })
+  })
+
+  router.get('/data/:organisationId/status', (req, res) => {
+    const organisation = req.session.data.user.organisations.filter(organisation => organisation.id = req.params.organisationId)[0]
+
+    const partners = req.session.data.relationships
+                    .filter(rel => rel.org1.id = req.params.organisationId)
+                    .map((relationship) => {
+                      return relationship.org2.name
+                    })
+
+    const statuses = [
+      'Received',
+      'Interviewing',
+      'Offered',
+      'Awaiting conditions',
+      'Ready to enroll'
+    ]
+
+    const statusData = StatisticsHelper.statusData
+
+    res.render('data/statistics/status', {
+      organisation,
       statuses,
       statusData,
       partners
@@ -56,7 +75,33 @@ module.exports = router => {
     const stageData = StatisticsHelper.progressData
 
     res.render('data/statistics/progress', {
-      organisation: req.query.organisation,
+      stages,
+      stageData,
+      partners
+    })
+  })
+
+  router.get('/data/:organisationId/progress', (req, res) => {
+    const organisation = req.session.data.user.organisations.filter(organisation => organisation.id = req.params.organisationId)[0]
+
+    const partners = req.session.data.relationships
+                    .filter(rel => rel.org1.id = req.params.organisationId)
+                    .map((relationship) => {
+                      return relationship.org2.name
+                    })
+
+    const stages = [
+      'From received to interviewing',
+      'From interviewing to offered',
+      'From offered to awaiting conditions',
+      'From awaiting conditions to ready to enroll',
+      'From offered to ready to enroll'
+    ]
+
+    const stageData = StatisticsHelper.progressData
+
+    res.render('data/statistics/progress', {
+      organisation,
       stages,
       stageData,
       partners
@@ -64,10 +109,6 @@ module.exports = router => {
   })
 
   router.get('/data/conversion', (req, res) => {
-    const userOrganisations = req.session.data.user.organisations.map((organisation) => {
-      return organisation.name
-    })
-
     const partners = req.session.data.relationships.map((relationship) => {
       return relationship.org2.name
     })
@@ -85,7 +126,35 @@ module.exports = router => {
     const conversionData = StatisticsHelper.conversionData
 
     res.render('data/statistics/conversion', {
-      organisation: req.query.organisation,
+      stages,
+      conversionData,
+      partners
+    })
+  })
+
+  router.get('/data/:organisationId/conversion', (req, res) => {
+    const organisation = req.session.data.user.organisations.filter(organisation => organisation.id = req.params.organisationId)[0]
+
+    const partners = req.session.data.relationships
+                    .filter(rel => rel.org1.id = req.params.organisationId)
+                    .map((relationship) => {
+                      return relationship.org2.name
+                    })
+
+    const stages = [
+      { title: 'Shortlist for interview rate', description: '% of applications received offered an interview' },
+      { title: 'Interview success rate', description: '% interviewed that are made an offer'},
+      { title: 'Offer rate', description: '% applications received that are made an offer'},
+      { title: 'Acceptance rate', description: '% offers made that are accepted'},
+      { title: 'Conditions met rate', description: '% accepted offers that go on to meet their conditions'},
+      { title: 'Offer conversion rate', description: '% offers made lead to successful enrollment'},
+      { title: 'Overall conversion', description: '% received applications that  lead to successful enrollment'}
+    ]
+
+    const conversionData = StatisticsHelper.conversionData
+
+    res.render('data/statistics/conversion', {
+      organisation,
       stages,
       conversionData,
       partners
