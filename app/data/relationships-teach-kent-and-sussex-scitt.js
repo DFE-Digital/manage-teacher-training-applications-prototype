4dc4b54bebf993f277b3f8521a8521d8f5f2cd47
Adam Silver <adambsilver@gmail.com>
2021-06-17 12:32:22 +0100

    Add back relationships

--- app/data/relationships-teach-kent-and-sussex-scitt.js
+++ app/data/relationships-teach-kent-and-sussex-scitt.js
@@ -17,50 +17,50 @@ relationships.push({
     viewDiversityInformation: true
   }
 })
-// relationships.push({
-//   id: 2,
-//   org1: userOrg,
-//   org1Permissions: {
-//     makeDecisions: true,
-//     viewSafeguardingInformation: true,
-//     viewDiversityInformation: true
-//   },
-//   org2: OrgHelper.findOrg("Claremont Primary School"),
-//   org2Permissions: {
-//     makeDecisions: true,
-//     viewSafeguardingInformation: true,
-//     viewDiversityInformation: true
-//   }
-// })
-// relationships.push({
-//   id: 3,
-//   org1: userOrg,
-//   org1Permissions: {
-//     makeDecisions: true,
-//     viewSafeguardingInformation: true,
-//     viewDiversityInformation: true
-//   },
-//   org2: OrgHelper.findOrg("Eastbourne Area School Direct Alliance"),
-//   org2Permissions: {
-//     makeDecisions: true,
-//     viewSafeguardingInformation: true,
-//     viewDiversityInformation: true
-//   }
-// })
-// relationships.push({
-//   id: 4,
-//   org1: userOrg,
-//   org1Permissions: {
-//     makeDecisions: true,
-//     viewSafeguardingInformation: true,
-//     viewDiversityInformation: true
-//   },
-//   org2: OrgHelper.findOrg("Wealden Partnership"),
-//   org2Permissions: {
-//     makeDecisions: true,
-//     viewSafeguardingInformation: true,
-//     viewDiversityInformation: true
-//   }
-// })
+relationships.push({
+  id: 2,
+  org1: userOrg,
+  org1Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  },
+  org2: OrgHelper.findOrg("Claremont Primary School"),
+  org2Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  }
+})
+relationships.push({
+  id: 3,
+  org1: userOrg,
+  org1Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  },
+  org2: OrgHelper.findOrg("Eastbourne Area School Direct Alliance"),
+  org2Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  }
+})
+relationships.push({
+  id: 4,
+  org1: userOrg,
+  org1Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  },
+  org2: OrgHelper.findOrg("Wealden Partnership"),
+  org2Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: true,
+    viewDiversityInformation: true
+  }
+})
 
 module.exports = relationships
