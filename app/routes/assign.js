a5a9a83d6a9f77971b3e7082ae80d33f29c6e578
Simon Whatley <simonwhatley@gmail.com>
2021-06-21 16:12:00 +0100

    Add application assignment

---
+++ app/routes/assign.js
@@ -0,0 +1,114 @@
+
+const parseUsers = (data, assignees, you) => {
+  if (!data) {
+    return null
+  }
+
+  const users = []
+
+  // get all users that aren't 'you'
+  data.forEach((item, i) => {
+    if (item.id !== you.id) {
+      const user = {}
+      user.value = item.id
+      user.text = item.firstName + ' ' + item.lastName
+
+      user.hint = {}
+      user.hint.text = item.emailAddress
+
+      user.checked = false
+      if (assignees && assignees.find(assignee => assignee.id === item.id) !== undefined) {
+        user.checked = true
+      }
+
+      users.push(user)
+    }
+  })
+
+  // sort the users alphabetically
+  users.sort((a, b) => a.text.localeCompare(b.text))
+
+  // put 'you' as the first person in the list of users
+  data.forEach((item, i) => {
+    if (item.id === you.id) {
+      const user = {}
+      user.value = item.id
+      user.text = item.firstName + ' ' + item.lastName + ' (you)'
+
+      user.hint = {}
+      user.hint.text = item.emailAddress
+
+      user.checked = false
+      if (assignees && assignees.find(assignee => assignee.id === item.id) !== undefined) {
+        user.checked = true
+      }
+
+      users.splice(0,0,user)
+    }
+  })
+
+  return users
+}
+
+module.exports = router => {
+
+  router.get('/applications/:applicationId/assign', (req, res) => {
+    const applicationId = req.params.applicationId
+    const application = req.session.data.applications.find(app => app.id === applicationId)
+
+    // get all the users for our organisation
+    let users = req.session.data.users.filter(user => {
+      return user.organisation.id == req.session.data.user.organisation.id
+    })
+
+    // parse users to an array we can use in the checkbox component
+    users = parseUsers(users, application.assignees, req.session.data.user)
+
+    res.render('applications/assign/index', {
+      application,
+      users
+    })
+  })
+
+  router.post('/applications/:applicationId/assign', (req, res) => {
+    const applicationId = req.params.applicationId
+    const application = req.session.data.applications.find(app => app.id === applicationId)
+
+    const assigneeIds = req.session.data.assignees
+
+    // get all the users for our organisation
+    const users = req.session.data.users.filter(user => {
+      return user.organisation.id == req.session.data.user.organisation.id
+    })
+
+    const assignees = []
+
+    if (assigneeIds) {
+      assigneeIds.forEach((assignee, i) => {
+        let user = {}
+        user = users.find(u => u.id === assignee)
+        assignees.push(user)
+      })
+    }
+
+    // add the assignees to the application
+    application.assignees = assignees
+
+    // clean up the session data before moving on
+    delete req.session.data.assignees
+
+    // TODO: we need to check if the application was previously assigned to
+    // ensure the success message is correct. For example, submitting an empty
+    // form shouldn't lead to an unassigned success message
+
+    if (assigneeIds && assigneeIds.length) {
+      req.flash('success', 'Application assigned')
+    } else {
+      req.flash('success', 'Application unassigned')
+    }
+
+    res.redirect(`/applications/${req.params.applicationId}/`);
+
+  })
+
+}
