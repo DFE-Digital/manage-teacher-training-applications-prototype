c734a37e4c100ea900ab22021607d8e57f9db544
Simon Whatley <simonwhatley@gmail.com>
2021-07-08 13:05:46 +0100

    Add Chiltern Training data

---
+++ app/data/relationships-chiltern-training.js
@@ -0,0 +1,28 @@
+const OrgHelper = require('./helpers/organisation')
+const relationships = []
+const userOrg = OrgHelper.findOrg("Chiltern Training Group")
+
+const partners = [
+  'Chiltern Training Group part of the Chiltern Learning trust'
+  'The Acorn Teaching School'
+]
+
+partners.forEach((partner, i) => {
+  relationships.push({
+    id: i,
+    org1: userOrg,
+    org1Permissions: {
+      makeDecisions: true,
+      viewSafeguardingInformation: false,
+      viewDiversityInformation: true
+    },
+    org2: OrgHelper.findOrg(partner),
+    org2Permissions: {
+      makeDecisions: true,
+      viewSafeguardingInformation: false,
+      viewDiversityInformation: false
+    }
+  })
+})
+
+module.exports = relationships
