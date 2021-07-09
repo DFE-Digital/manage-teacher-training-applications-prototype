f0ab827e48d30a83d1430e96805e585317312383
Simon Whatley <simonwhatley@gmail.com>
2021-07-09 10:54:43 +0100

    Update relationships

--- app/data/relationships-cambridge.js
+++ app/data/relationships-cambridge.js
@@ -2,20 +2,20 @@ const OrgHelper = require('./helpers/organisation')
 const relationships = []
 const userOrg = OrgHelper.findOrg("University of Cambridge")
 
-// relationships.push({
-//   id: 1,
-//   org1: userOrg,
-//   org1Permissions: {
-//     makeDecisions: true,
-//     viewSafeguardingInformation: false,
-//     viewDiversityInformation: true
-//   },
-//   org2: OrgHelper.findOrg("ORGANISATION NAME"),
-//   org2Permissions: {
-//     makeDecisions: true,
-//     viewSafeguardingInformation: false,
-//     viewDiversityInformation: false
-//   }
-// })
+relationships.push({
+  id: 1,
+  org1: userOrg,
+  org1Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: false,
+    viewDiversityInformation: true
+  },
+  org2: OrgHelper.findOrg("University of Cambridge"),
+  org2Permissions: {
+    makeDecisions: true,
+    viewSafeguardingInformation: false,
+    viewDiversityInformation: false
+  }
+})
 
 module.exports = relationships
