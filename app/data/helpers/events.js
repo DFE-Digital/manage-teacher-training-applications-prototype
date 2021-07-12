bbcb882943b874a367690863fe07f94b9a457673
Simon Whatley <simonwhatley@gmail.com>
2021-07-12 15:56:32 +0100

    Update assigned users sort order

--- app/data/helpers/events.js
+++ app/data/helpers/events.js
@@ -8,10 +8,15 @@ exports.saveEvent = (application, event) => {
   const date = DateTime.now().toJSDate()
   const events = application.events
 
+  const assignedUsers = event.assignedUsers
+    .sort((a, b) => a.firstName.localeCompare(b.firstName)
+      || a.lastName.localeCompare(b.lastName)
+      || a.emailAddress.localeCompare(b.emailAddress))
+
   events.items.push({
     title: event.title,
     user: event.user,
-    assignedUsers: event.assignedUsers,
+    assignedUsers,
     date
   })
 }
