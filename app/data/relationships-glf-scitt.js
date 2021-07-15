ac81af6fa3f3d95cfb62cdda1ac836415bcd7802
Simon Whatley <simonwhatley@gmail.com>
2021-07-15 09:48:33 +0100

    Update relationships

--- app/data/relationships-glf-scitt.js
+++ app/data/relationships-glf-scitt.js
@@ -2,20 +2,27 @@ const OrgHelper = require('./helpers/organisation')
 const relationships = []
 const userOrg = OrgHelper.findOrg("GLF School’s Teacher Training")
 
-relationships.push({
-  id: 1,
-  org1: userOrg,
-  org1Permissions: {
-    makeDecisions: true,
-    viewSafeguardingInformation: false,
-    viewDiversityInformation: true
-  },
-  org2: OrgHelper.findOrg("Glyn School"),
-  org2Permissions: {
-    makeDecisions: true,
-    viewSafeguardingInformation: false,
-    viewDiversityInformation: false
-  }
+const partners = [
+  'GLF School’s Teacher Training',
+  'Glyn School'
+]
+
+partners.forEach((partner, i) => {
+  relationships.push({
+    id: i,
+    org1: userOrg,
+    org1Permissions: {
+      makeDecisions: true,
+      viewSafeguardingInformation: false,
+      viewDiversityInformation: true
+    },
+    org2: OrgHelper.findOrg(partner),
+    org2Permissions: {
+      makeDecisions: true,
+      viewSafeguardingInformation: false,
+      viewDiversityInformation: false
+    }
+  })
 })
 
 module.exports = relationships
