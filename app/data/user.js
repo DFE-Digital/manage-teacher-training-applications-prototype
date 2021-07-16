51f2333efedf3f7d1a99ad46f40d249896a4cb52
Simon Whatley <simonwhatley@gmail.com>
2021-07-16 10:55:42 +0100

    Update relationships

--- app/data/user.js
+++ app/data/user.js
@@ -1,10 +1,12 @@
 const OrgHelper = require('./helpers/organisation')
 const users = require('./users.json')
-let userOrg = OrgHelper.findOrg("Ignite Teaching School")
+const userOrg1 = OrgHelper.findOrg("Ignite Teaching School")
+const userOrg2 = OrgHelper.findOrg("The OAKS Norfolk")
 
 // set up user orgs
 let userOrgs = [];
-userOrgs.push(userOrg)
+userOrgs.push(userOrg1)
+userOrgs.push(userOrg2)
 
 // create user object
 let user = users[0] // we know the first one is the test participant
