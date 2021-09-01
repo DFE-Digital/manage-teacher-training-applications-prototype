c693c866dd44a0ccba359d67e0b00245aaa54170
Simon Whatley <simonwhatley@gmail.com>
2021-09-01 11:42:02 +0100

    Reorganise data route

--- app/routes/data.js
+++ app/routes/data.js
@@ -25,16 +25,6 @@ const statuses = [
   { code: 'recruited', title: 'Recruited' }
 ]
 
-const stages = [
-  { code: 'shortlist_for_interview', title: 'Invited to interview', description: 'Applications which led to interviews' },
-  { code: 'interview_success', title: 'Made offer after interview', description: 'Interviews which led to offers'},
-  { code: 'offer', title: 'Made offer', description: 'Applications which led to offers'},
-  { code: 'acceptance', title: 'Accepted offer', description: 'Offers which led to candidate accepting'},
-  { code: 'conditions_met', title: 'Met offer conditions', description: 'Accepted offers which led to conditions being met'},
-  { code: 'offer_conversion', title: 'Successful offer', description: 'Offers which led to candidate being recruited'},
-  { code: 'overall_conversion', title: 'Successful application', description: 'Applications which led to candidate being recruited'}
-]
-
 module.exports = router => {
 
   router.get('/reports', (req, res) => {
@@ -63,6 +53,56 @@ module.exports = router => {
     })
   })
 
+  router.get('/reports/:organisationId/status-of-applications/download', (req, res) => {
+    const fileName = '/status-of-applications-' + DateTime.now().toFormat('yyyy-LL-dd-HH-mm-ss') + '.csv'
+    const filePath = downloadDirectoryPath + fileName
+
+    const organisation = req.session.data.user.organisations.find(org => org.id === req.params.organisationId)
+
+    const organisationName = slugify(organisation.name)
+
+    const statusData = StatisticsHelper.getStatusData(organisationName)
+
+    // headers for the CSV file
+    const headers = []
+    headers.push({ id: 'course', title: 'Course' })
+    headers.push({ id: 'code', title: 'Course code' })
+    headers.push({ id: 'provider', title: 'Partner organisation' })
+
+    statuses.forEach((status, i) => {
+      const header = {}
+      header.id = status.code + '_number'
+      header.title = status.title // + ' - number'
+      headers.push(header)
+    })
+
+    const csv = csvWriter({
+      path: filePath,
+      header: headers
+    })
+
+    const records = []
+
+    statusData.forEach((item, i) => {
+      const data = {}
+
+      data.course = item.title
+      data.code = item.code
+      data.provider = item.provider
+
+      statuses.forEach((status, i) => {
+        data[status.code + '_number'] = item[status.code].number
+      })
+
+      records.push(data)
+    })
+
+    csv.writeRecords(records)
+      .then(() => {
+        res.download(filePath,fileName)
+      })
+  })
+
   router.get('/reports/:organisationId/candidate-drop-out', (req, res) => {
     const organisation = req.session.data.user.organisations.find(org => org.id === req.params.organisationId)
 
@@ -72,7 +112,6 @@ module.exports = router => {
 
     res.render('data/statistics/attrition/index', {
       organisation,
-      stages,
       attritionData
     })
   })
@@ -139,15 +178,6 @@ module.exports = router => {
   router.get('/reports/:organisationId/candidate-drop-out/:courseId', (req, res) => {
     const organisation = req.session.data.user.organisations.find(org => org.id === req.params.organisationId)
 
-    const statuses = [
-      { code: 'rejections', title: 'Applications that led to rejection', description: '' },
-      { code: 'interview_rejections', title: 'Interviews that led to rejection', description: ''},
-      { code: 'applications_withdrawn', title: 'Applications that led to being withdrawn', description: ''},
-      { code: 'offers_withdrawn', title: 'Offers that were withdrawn', description: ''},
-      { code: 'offers_declined', title: 'Offers that were declined', description: ''},
-      { code: 'conditions_not_met', title: 'Accepted offers that led to candidates not meeting one or more conditions', description: ''}
-    ]
-
     const fileName = slugify(organisation.name)
 
     let course = StatisticsHelper.getAttritionData(fileName)
@@ -172,56 +202,6 @@ module.exports = router => {
     res.render('data/export/hesa', {
       organisation
     })
-  })
-
-  router.get('/reports/:organisationId/status-of-applications/download', (req, res) => {
-    const fileName = '/status-of-applications-' + DateTime.now().toFormat('yyyy-LL-dd-HH-mm-ss') + '.csv'
-    const filePath = downloadDirectoryPath + fileName
-
-    const organisation = req.session.data.user.organisations.find(org => org.id === req.params.organisationId)
-
-    const organisationName = slugify(organisation.name)
-
-    const statusData = StatisticsHelper.getStatusData(organisationName)
-
-    // headers for the CSV file
-    const headers = []
-    headers.push({ id: 'course', title: 'Course' })
-    headers.push({ id: 'code', title: 'Course code' })
-    headers.push({ id: 'provider', title: 'Partner organisation' })
-
-    statuses.forEach((status, i) => {
-      const header = {}
-      header.id = status.code + '_number'
-      header.title = status.title // + ' - number'
-      headers.push(header)
-    })
-
-    const csv = csvWriter({
-      path: filePath,
-      header: headers
-    })
-
-    const records = []
-
-    statusData.forEach((item, i) => {
-      const data = {}
-
-      data.course = item.title
-      data.code = item.code
-      data.provider = item.provider
-
-      statuses.forEach((status, i) => {
-        data[status.code + '_number'] = item[status.code].number
-      })
-
-      records.push(data)
-    })
-
-    csv.writeRecords(records)
-      .then(() => {
-        res.download(filePath,fileName)
-      })
   })
 
 }
