56d002e90faa78ac884c01bb03cead7b2abfd650
Simon Whatley <simonwhatley@gmail.com>
2021-09-15 10:35:13 +0100

    Change prototype provider to the Wren Academy default

--- app/data/user.js
+++ app/data/user.js
@@ -1,11 +1,9 @@
 const OrgHelper = require('./helpers/organisation')
 const users = require('./users.json')
-// const userOrg = OrgHelper.findOrg("Ignite Teaching School")
 
 // set up user orgs
 let userOrgs = [];
-userOrgs.push(OrgHelper.findOrg("Ignite Teaching School"))
-userOrgs.push(OrgHelper.findOrg("The OAKS Norfolk"))
+userOrgs.push(OrgHelper.findOrg('Wren Academy'))
 
 // create user object
 let user = users[0] // we know the first one is the test participant
