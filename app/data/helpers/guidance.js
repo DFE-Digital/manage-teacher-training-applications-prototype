cc2b49125b43fb7c08c65cc043f6af23c1ab193e
Simon Whatley <simonwhatley@gmail.com>
2021-09-30 11:49:27 +0100

    Rename gray matter variable

--- app/data/helpers/guidance.js
+++ app/data/helpers/guidance.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const fs = require('fs')
-const markdown = require('gray-matter')
+const matter = require('gray-matter')
 
 const directoryPath = path.join(__dirname, '../../views/guidance/_content/')
 
 exports.getMarkdownContent = (fileName) => {
   let doc = fs.readFileSync(directoryPath + fileName + '.md', 'utf8')
-  const content = markdown(doc)
+  const content = matter(doc)
   return content
 }
 
@@ -20,7 +20,7 @@ exports.getListOfFiles = () => {
 
   documents.forEach((filename, i) => {
     const doc = fs.readFileSync(directoryPath + '/' + filename)
-    const content = markdown(doc)
+    const content = matter(doc)
     const file = {}
     file.slug = filename.replace(/.(md)/,'')
     file.title = content.data.title
