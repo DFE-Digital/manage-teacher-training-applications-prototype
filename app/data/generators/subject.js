916c756038387a13178456b7c1c9077f8d172fe8
Simon Whatley <23444+simonwhatley@users.noreply.github.com>
2021-10-06 10:58:41 +0100

    Remove unused data generators (#465)
    Remove subject and training location generators

--- app/data/generators/subject.js
+++
@@ -1,8 +0,0 @@
-const faker = require('faker')
-faker.locale = 'en_GB'
-const subjects = require('../subjects.json')
-
-module.exports = () => {
-  const subject = faker.helpers.randomize(subjects)
-  return subject
-}
