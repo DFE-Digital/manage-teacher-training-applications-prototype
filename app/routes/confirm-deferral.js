a9878e218c783a18d60a5a34b2cffe5536ffc8ac
Adam Silver <adambsilver@gmail.com>
2021-11-15 11:27:32 +0000

    Update on deferals

--- app/routes/confirm-deferral.js
+++ app/routes/confirm-deferral.js
@@ -1,12 +1,42 @@
 const ApplicationHelper = require('../data/helpers/application')
 const CycleHelper = require('../data/helpers/cycles')
+const { v4: uuidv4 } = require('uuid')
 
 module.exports = router => {
 
+
+
   router.get('/applications/:applicationId/offer/reconfirm/check', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-    const conditions = application.offer.standardConditions.concat(application.offer.conditions)
+    let application = req.session.data.applications.find(app => app.id === req.params.applicationId)
+
+    var conditions = []
+
+    // if it's been submitted then build conditions from data
+    if(req.session.data['confirm-offer'] && req.session.data['confirm-offer']['submitted-conditions-page'] == 'true') {
+
+      // standard conditions
+      if(req.session.data['confirm-offer']['standard-conditions'] && req.session.data['confirm-offer']['standard-conditions'].length) {
+        conditions = conditions.concat(req.session.data['confirm-offer']['standard-conditions'])
+      }
+
+      if(req.session.data['confirm-offer']['conditions'] && req.session.data['confirm-offer']['conditions'].length) {
+        req.session.data['confirm-offer']['conditions'].filter(c => c != '').forEach(c => {
+          conditions.push(c)
+        })
+      }
+
+      conditions = conditions.map(c => {
+        return {
+          description: c,
+          status: "Pending"
+        }
+      })
+
+    // not submitted, build from application.offer object
+    } else {
+      conditions = ApplicationHelper.getConditions(application.offer)
+    }
+
     res.render('applications/offer/confirm-deferral/check', {
       application,
       conditions
@@ -17,25 +47,60 @@ module.exports = router => {
     const applicationId = req.params.applicationId
     const application = req.session.data.applications.find(app => app.id === applicationId)
 
-    application.offer.madeDate = new Date().toISOString()
-    application.status = 'Conditions pending' // work this out
-    application.cycle = CycleHelper.CURRENT_CYCLE.code
+    application.offer.provider = req.session.data['confirm-offer'].provider || application.offer.provider
+    application.offer.course = req.session.data['confirm-offer'].course || application.offer.course
+    application.offer.location = req.session.data['confirm-offer'].location || application.offer.location
+    application.offer.studyMode = req.session.data['confirm-offer'].studyMode || application.offer.studyMode
+
+    // if it's been submitted then save conditions from data
+    if(req.session.data['confirm-offer'] && req.session.data['confirm-offer']['submitted-conditions-page'] == 'true') {
+      // save standard conditions
+      application.offer.standardConditions = [];
+      if(req.session.data['confirm-offer']['standard-conditions'] && req.session.data['confirm-offer']['standard-conditions'].length) {
+        req.session.data['confirm-offer']['standard-conditions'].forEach(condition => {
+          application.offer.standardConditions.push({
+            id: uuidv4(),
+            description: condition,
+            status: "Pending"
+          })
+        });
+      }
+
+      // save further conditions
+      application.offer.conditions = [];
+
+      req.session.data['confirm-offer']['conditions'].filter(c => c != '').forEach(c => {
+        application.offer.conditions.push({
+          id: uuidv4(),
+          description: c,
+          status: "Pending"
+        })
+      })
+    }
 
     ApplicationHelper.addEvent(application, {
+      title: "Deferred offer confirmed",
+      user: "Ben Brown",
       date: new Date().toISOString(),
-      user: "Alicia Grenada",
-      title: "Offer reconfirmed",
       meta: {
         offer: {
           provider: application.offer.provider,
           course: application.offer.course,
           location: application.offer.location,
+          studyMode: application.offer.studyMode,
           accreditedBody: application.offer.accreditedBody,
-          conditions: application.offer.standardConditions.concat(application.offer.conditions)
+          conditions: ApplicationHelper.getConditions(application.offer)
         }
       }
     })
-    req.flash('success', 'Deferred offer successfully confirmed for current cycle')
+
+    application.offer.madeDate = new Date().toISOString()
+    application.status = 'Conditions pending' // work this out
+    application.cycle = CycleHelper.CURRENT_CYCLE.code
+
+    delete req.session.data['confirm-offer']
+
+    req.flash('success', 'Deferred offer confirmed')
     res.redirect(`/applications/${applicationId}/offer`)
   })
 
@@ -59,19 +124,46 @@ module.exports = router => {
     const applicationId = req.params.applicationId
     const application = req.session.data.applications.find(app => app.id === applicationId)
     res.render('applications/offer/confirm-deferral/unavailable', {
-      application
+      application,
+      conditions: ApplicationHelper.getConditions(application.offer)
     })
   })
 
   router.get('/applications/:applicationId/offer/reconfirm/conditions', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-    application.offerAvailable = true
+    let application = req.session.data.applications.find(app => app.id === req.params.applicationId)
+    let standardConditions;
+    let conditions;
 
-    res.render('applications/offer/reconfirm/conditions', {
-      application: application,
-      standardConditions: application.offer.standardConditions,
-      furtherConditions: application.offer.conditions
+    if(!req.session.data['confirm-offer'] || !req.session.data['confirm-offer']['standard-conditions']) {
+
+      if(application.offer.standardConditions) {
+        standardConditions = application.offer.standardConditions.map(condition => {
+          return condition.description
+        })
+      }
+
+    }
+
+    // cleanse data gah
+    if(req.session.data['confirm-offer'] && req.session.data['confirm-offer']['conditions']) {
+      req.session.data['confirm-offer']['conditions'] = req.session.data['confirm-offer']['conditions'].filter(c => c != '')
+    }
+
+    // if the form has been used in some way
+    if(req.session.data['confirm-offer'] && req.session.data['confirm-offer']['submitted-conditions-page'] == 'true') {
+      conditions = req.session.data['confirm-offer']['conditions']
+    } else {
+      if(application.offer.conditions) {
+        conditions = application.offer.conditions.map(c => {
+          return c.description
+        })
+      }
+    }
+
+    res.render('applications/offer/confirm-deferral/conditions', {
+      application,
+      standardConditions,
+      conditions
     })
   })
 
@@ -79,23 +171,27 @@ module.exports = router => {
     res.redirect(`/applications/${req.params.applicationId}/offer/reconfirm/check`)
   })
 
-  /*************************************************************
-   *
-   * Unavailable location
-   *
-   ****************************************************************/
-
-  router.get('/applications/:applicationId/offer/reconfirm/unavailable-location/statuses', (req, res) => {
+  router.get('/applications/:applicationId/offer/reconfirm/statuses', (req, res) => {
     const applicationId = req.params.applicationId
     const application = req.session.data.applications.find(app => app.id === applicationId)
-    const conditions = application.offer.standardConditions.concat(application.offer.conditions)
-    res.render('applications/offer/reconfirm/unavailable-location/statuses', {
+
+    let conditions = []
+
+    if(application.offer.standardConditions) {
+      conditions = conditions.concat(application.offer.standardConditions)
+    }
+
+    if(application.offer.conditions) {
+      conditions = conditions.concat(application.offer.conditions)
+    }
+
+    res.render('applications/offer/confirm-deferral/statuses', {
       application,
       conditions
     })
   })
 
-  router.post('/applications/:applicationId/offer/reconfirm/unavailable-location/statuses', (req, res) => {
+  router.post('/applications/:applicationId/offer/reconfirm/statuses', (req, res) => {
     const applicationId = req.params.applicationId
     const data = req.session.data
     const application = req.session.data.applications.find(app => app.id === applicationId)
@@ -110,192 +206,9 @@ module.exports = router => {
       delete data.conditionStatus
       conditions.forEach( (condition, index) => condition.status = newConditionStatuses[index])
     }
-    res.redirect(`/applications/${applicationId}/offer/reconfirm/unavailable-location/check`)
+    res.redirect(`/applications/${applicationId}/offer/reconfirm/check`)
   })
 
-  router.get('/applications/:applicationId/offer/reconfirm/unavailable-location/conditions', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
 
-    res.render('applications/offer/reconfirm/unavailable-location/conditions', {
-      application: application,
-      standardConditions: application.offer.standardConditions,
-      furtherConditions: application.offer.conditions
-    })
-  })
-
-  router.post('/applications/:applicationId/offer/reconfirm/unavailable-location/conditions', (req, res) => {
-    res.redirect(`/applications/${req.params.applicationId}/offer/reconfirm/unavailable-location/check`)
-  })
-
-  router.get('/applications/:applicationId/offer/reconfirm/unavailable-location/check', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-    const conditions = application.offer.standardConditions.concat(application.offer.conditions)
-    res.render('applications/offer/reconfirm/unavailable-location/check', {
-      application: application,
-      conditions: conditions
-    })
-  })
-
-  router.post('/applications/:applicationId/offer/reconfirm/unavailable-location/check', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-
-    application.offer.madeDate = new Date().toISOString()
-    application.offer.provider = req.session.data.trainingProviders[0].name
-    application.offer.course = 'Primary (5-11) (X100)'
-    application.offer.location = req.session.data.location
-
-    application.status = 'Recruited' // work this out
-    application.cycle = CycleHelper.CURRENT_CYCLE.code
-    application.events.items.push({
-      date: new Date().toISOString(),
-      user: "Alicia Grenada",
-      title: "Offer reconfirmed"
-    })
-
-    req.flash('success', 'Deferred offer successfully confirmed for current cycle')
-    res.redirect(`/applications/${applicationId}/offer`)
-  })
-
-  /*************************************************************
-   *
-   * Unavailable course
-   *
-   ****************************************************************/
-
-  router.get('/applications/:applicationId/offer/reconfirm/unavailable-course', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-    const conditions = application.offer.standardConditions.concat(application.offer.conditions)
-
-    res.render('applications/offer/reconfirm/unavailable-course/action', {
-      application,
-      conditions: conditions
-    })
-  })
-
-  router.post('/applications/:applicationId/offer/reconfirm/unavailable-course', (req, res) => {
-    res.redirect(`/applications/${req.params.applicationId}/offer/reconfirm/unavailable-course/course`)
-  })
-
-  router.get('/applications/:applicationId/offer/reconfirm/unavailable-course/provider', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-    const conditions = application.offer.standardConditions.concat(application.offer.conditions)
-
-    res.render('applications/offer/reconfirm/unavailable-course/provider', {
-      application: application,
-      conditions: conditions
-    })
-  })
-
-  router.post('/applications/:applicationId/offer/reconfirm/unavailable-course/provider', (req, res) => {
-    res.redirect(`/applications/${req.params.applicationId}/offer/reconfirm/unavailable-course/course`)
-  })
-
-  router.get('/applications/:applicationId/offer/reconfirm/unavailable-course/course', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-    const conditions = application.offer.standardConditions.concat(application.offer.conditions)
-    res.render('applications/offer/reconfirm/unavailable-course/course', {
-      application: application,
-      conditions: conditions
-    })
-  })
-
-  router.post('/applications/:applicationId/offer/reconfirm/unavailable-course/course', (req, res) => {
-    res.redirect(`/applications/${req.params.applicationId}/offer/reconfirm/unavailable-course/location`)
-  })
-
-  router.get('/applications/:applicationId/offer/reconfirm/unavailable-course/location', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-    const conditions = application.offer.standardConditions.concat(application.offer.conditions)
-
-    res.render('applications/offer/reconfirm/unavailable-course/location', {
-      application: application,
-      conditions: conditions
-    })
-  })
-
-  router.post('/applications/:applicationId/offer/reconfirm/unavailable-course/location', (req, res) => {
-    res.redirect(`/applications/${req.params.applicationId}/offer/reconfirm/unavailable-course/statuses`)
-  })
-
-  router.get('/applications/:applicationId/offer/reconfirm/unavailable-course/statuses', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-    const conditions = application.offer.standardConditions.concat(application.offer.conditions)
-    res.render('applications/offer/reconfirm/unavailable-course/statuses', {
-      application,
-      conditions
-    })
-  })
-
-  router.post('/applications/:applicationId/offer/reconfirm/unavailable-course/statuses', (req, res) => {
-    const applicationId = req.params.applicationId
-    const data = req.session.data
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-    const conditions = ApplicationHelper.getConditions(application.offer)
-    if (data.allConditionsMet){
-      let allConditionsMet = (data.allConditionsMet == 'true') ? true : false
-      delete data.allConditionsMet
-      conditions.forEach( (condition, index) => condition.status = (allConditionsMet)? 'Met' : 'Pending')
-    }
-    if (data.conditionStatus){
-      let newConditionStatuses = data.conditionStatus
-      delete data.conditionStatus
-      conditions.forEach( (condition, index) => condition.status = newConditionStatuses[index])
-    }
-    res.redirect(`/applications/${applicationId}/offer/reconfirm/unavailable-course/check`)
-  })
-
-  router.get('/applications/:applicationId/offer/reconfirm/unavailable-course/conditions', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-
-    res.render('applications/offer/reconfirm/unavailable-course/conditions', {
-      application: application,
-      standardConditions: application.offer.standardConditions,
-      furtherConditions: application.offer.conditions
-    })
-  })
-
-  router.post('/applications/:applicationId/offer/reconfirm/unavailable-course/conditions', (req, res) => {
-    res.redirect(`/applications/${req.params.applicationId}/offer/reconfirm/unavailable-course/check`)
-  })
-
-  router.get('/applications/:applicationId/offer/reconfirm/unavailable-course/check', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-    const conditions = application.offer.standardConditions.concat(application.offer.conditions)
-    res.render('applications/offer/reconfirm/unavailable-course/check', {
-      application: application,
-      conditions: conditions
-    })
-  })
-
-  router.post('/applications/:applicationId/offer/reconfirm/unavailable-course/check', (req, res) => {
-    const applicationId = req.params.applicationId
-    const application = req.session.data.applications.find(app => app.id === applicationId)
-
-    application.offer.madeDate = new Date().toISOString();
-    application.offer.provider =  req.session.data.trainingProviders[0].name;
-    application.offer.course = 'Primary (5-11) (X100)';
-    application.offer.location = req.session.data.location;
-
-    application.status = 'Recruited' // work this out
-    application.cycle = CycleHelper.CURRENT_CYCLE.code
-    application.events.items.push({
-      date: new Date().toISOString(),
-      user: "Alicia Grenada",
-      title: "Offer reconfirmed"
-    })
-
-    req.flash('success', 'Deferred offer successfully confirmed for current cycle')
-    res.redirect(`/applications/${applicationId}/offer`)
-  })
 
 }
