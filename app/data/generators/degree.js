56439a512b884cd06a48264e576b153e58dde987
Simon Whatley <simonwhatley@gmail.com>
2021-11-17 11:23:45 +0000

    Update degree helper to correctly use dates

--- app/data/generators/degree.js
+++ app/data/generators/degree.js
@@ -7,11 +7,11 @@ const degreeData = require('../degree')
 
 module.exports = (isInternationCandidate, dateOfBirth) => {
 
-  // Start year - 18 years after date of birth
+  // Start year - 18/19 years after date of birth
   let year = DateTime.fromISO(dateOfBirth).toObject().year
-  year += 18
+  year += faker.helpers.randomize([18,19])
 
-  const nowYear = DateTime.now().year
+  const currentYear = DateTime.now().year
 
   // 80% of candidates have 1 degree
   const count = weighted.select({
@@ -21,22 +21,23 @@ module.exports = (isInternationCandidate, dateOfBirth) => {
 
   const degrees = []
 
-  // TODO: start year for second degree must be after first
   for (let i = 0; i < count; i++) {
     let degree = {}
 
     degree.subject = faker.helpers.randomize(degreeData().subjects)
 
+    // Start year for second degree must be after first
     if (i > 0) {
       degree.startYear = degrees[i-1].startYear + faker.datatype.number({ 'min': 1, 'max': 10 })
     } else {
       degree.startYear = year
     }
 
+    // Set the graduation year to be between 3 and 4 years after start year
     degree.graduationYear = degree.startYear + faker.datatype.number({ 'min': 3, 'max': 4 })
 
-    // If graduationYear after this year, set predicted true
-    degree.predicted = (degree.graduationYear > nowYear) ? true : false
+    // If graduation year is after the current year, set predicted to true
+    degree.predicted = (degree.graduationYear > currentYear) ? true : false
 
     if (isInternationCandidate) {
 
@@ -52,26 +53,34 @@ module.exports = (isInternationCandidate, dateOfBirth) => {
     } else {
 
       const type = faker.helpers.randomize(degreeData().types.all)
-      const level = type.level
-      const grade = faker.helpers.randomize([
+
+      degree.level = type.level
+
+      degree.grade = faker.helpers.randomize([
         'First-class honours',
         'Upper second-class honours (2:1)',
         'Lower second-class honours (2:2)',
         'Third-class honours',
         'Pass',
-        ...(level !== 6) ? ['Merit'] : [],
-        ...(level !== 6) ? ['Distinction'] : [],
-        ...(level !== 6) ? ['Not applicable'] : [],
-        ...(level !== 6) ? ['Unknown'] : []
+        ...(degree.level !== 6) ? ['Merit'] : [],
+        ...(degree.level !== 6) ? ['Distinction'] : [],
+        ...(degree.level !== 6) ? ['Not applicable'] : [],
+        ...(degree.level !== 6) ? ['Unknown'] : []
       ])
 
-      degree.type = type.value + (grade.includes('honours') ? ' (Hons)' : '')
+      degree.type = type.text + (degree.grade.includes('honours') ? ' (Hons)' : '')
+
       degree.institution = faker.helpers.randomize(degreeData().orgs)
       degree.country = 'United Kingdom'
     }
 
     degrees.push(degree)
   }
+
+  // sort the degrees in reverse chronological order
+  // degrees.sort((a, b) => {
+  //   return b.startYear - a.startYear
+  // })
 
   return degrees
 }
