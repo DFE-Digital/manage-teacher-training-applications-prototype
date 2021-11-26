2f7de26af6c3e330fbad942bc347187d3c30689a
Simon Whatley <simonwhatley@gmail.com>
2021-11-26 12:13:36 +0000

    Add complaints page

--- app/routes/feedback.js
+++ app/routes/feedback.js
@@ -4,8 +4,8 @@ const directoryPath = path.join(__dirname, '../views/feedback/_content/')
 const MarkdownHelper = require('../data/helpers/markdown')
 
 module.exports = router => {
-  router.get('/feedback', (req, res) => {
-    const markdown = MarkdownHelper.getMarkdownContent(directoryPath, 'feedback')
+  router.get('/complaints', (req, res) => {
+    const markdown = MarkdownHelper.getMarkdownContent(directoryPath, 'complaints')
 
     res.render('feedback/index', {
       title: markdown.data.title,
