081a63947fcbd01724b64a7c556291ed5032606c
Simon Whatley <simonwhatley@gmail.com>
2021-11-26 17:03:42 +0000

    Update complaint content

--- app/routes/feedback.js
+++ app/routes/feedback.js
@@ -1,3 +1,6 @@
+const faker = require('faker')
+faker.locale = 'en_GB'
+
 const path = require('path')
 const directoryPath = path.join(__dirname, '../views/feedback/_content/')
 
@@ -5,11 +8,15 @@ const MarkdownHelper = require('../data/helpers/markdown')
 
 module.exports = router => {
   router.get('/complaints', (req, res) => {
-    const markdown = MarkdownHelper.getMarkdownContent(directoryPath, 'complaints')
+
+    // TODO: dynamically parse chat content on page
+    const chatStatus = faker.helpers.randomize(['online', 'offline', 'unavailable'])
+    const markdown = MarkdownHelper.getMarkdownContent(directoryPath, 'complaints-' + chatStatus)
 
     res.render('feedback/index', {
       title: markdown.data.title,
-      content: markdown.content
+      content: markdown.content,
+      chatStatus
     })
 
   })
---
+++ app/views/feedback/_content/complaints-online.md
@@ -0,0 +1,15 @@
+---
+title: Make a complaint
+---
+
+You can make a complaint by email or online chat.
+
+## Email
+
+Send an email to becomingateacher@digital.education.gov.uk.
+
+We’ll respond within 5 working days.
+
+## Online chat
+
+[Speak to an adviser now (opens in a new window)](#)
