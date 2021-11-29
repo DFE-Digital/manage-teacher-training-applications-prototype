3f7603e5333f5ebb7a2a0e9cb4c80a9afab5c710
Simon Whatley <simonwhatley@gmail.com>
2021-11-29 14:55:56 +0000

    Remove complaints content

--- app/routes/feedback.js
+++ app/routes/feedback.js
@@ -7,17 +7,17 @@ const directoryPath = path.join(__dirname, '../views/feedback/_content/')
 const MarkdownHelper = require('../data/helpers/markdown')
 
 module.exports = router => {
-  router.get('/complaints', (req, res) => {
-
-    // TODO: dynamically parse chat content on page
-    const chatStatus = faker.helpers.randomize(['online', 'offline', 'unavailable'])
-    const markdown = MarkdownHelper.getMarkdownContent(directoryPath, 'complaints-' + chatStatus)
-
-    res.render('feedback/index', {
-      title: markdown.data.title,
-      content: markdown.content,
-      chatStatus
-    })
-
-  })
+  // router.get('/complaints', (req, res) => {
+  //
+  //   // TODO: dynamically parse chat content on page
+  //   const chatStatus = faker.helpers.randomize(['online', 'offline', 'unavailable'])
+  //   const markdown = MarkdownHelper.getMarkdownContent(directoryPath, 'complaints-' + chatStatus)
+  //
+  //   res.render('feedback/index', {
+  //     title: markdown.data.title,
+  //     content: markdown.content,
+  //     chatStatus
+  //   })
+  //
+  // })
 }
