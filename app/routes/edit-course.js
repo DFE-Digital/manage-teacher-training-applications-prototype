d4e7b6dca50bc4fde393a5fe485eea990acbdc44
Simon Whatley <simonwhatley@gmail.com>
2021-12-01 15:28:38 +0000

    Update edit-course.js

--- app/routes/edit-course.js
+++ app/routes/edit-course.js
@@ -1,15 +1,49 @@
+const Utils = require('../data/helpers/utils')
+
+const locations = require('../data/locations')
+
+const getLocationItems = (selectedItems) => {
+  const items = []
+
+  locations.forEach((location, i) => {
+    const item = {}
+
+    let address = Object.values(location.address)
+    // hack to remove empty items from address
+    address = address.filter(item => item !== '')
+    address = Utils.arrayToList(array = address, join = ', ', final = ', ')
+
+    const value = location.name + ', ' + address
+
+    item.text = location.name
+    item.value = value
+    item.id = location.code
+    item.checked = (selectedItems && selectedItems.includes(value)) ? 'checked' : ''
+
+    item.hint = {}
+    item.hint.text = address
+
+    items.push(item)
+  })
+
+  items.sort((a,b) => {
+    return a.text.localeCompare(b.text)
+  })
+
+  return items
+}
 
 module.exports = router => {
 
-  router.get('/applications/:applicationId/course/edit/provider', (req, res) => {
-    res.render('applications/course/provider', {
-      application: req.session.data.applications.find(app => app.id === req.params.applicationId)
-    })
-  })
-
-  router.post('/applications/:applicationId/course/edit/provider', (req, res) => {
-    res.redirect(`/applications/${req.params.applicationId}/course/edit/course`)
-  })
+  // router.get('/applications/:applicationId/course/edit/provider', (req, res) => {
+  //   res.render('applications/course/provider', {
+  //     application: req.session.data.applications.find(app => app.id === req.params.applicationId)
+  //   })
+  // })
+  //
+  // router.post('/applications/:applicationId/course/edit/provider', (req, res) => {
+  //   res.redirect(`/applications/${req.params.applicationId}/course/edit/course`)
+  // })
 
   router.get('/applications/:applicationId/course/edit/course', (req, res) => {
     res.render('applications/course/course', {
@@ -18,6 +52,7 @@ module.exports = router => {
   })
 
   router.post('/applications/:applicationId/course/edit/course', (req, res) => {
+    console.log(req.session.data['edit-course'])
     res.redirect(`/applications/${req.params.applicationId}/course/edit/study-mode`)
   })
 
@@ -28,16 +63,24 @@ module.exports = router => {
   })
 
   router.post('/applications/:applicationId/course/edit/study-mode', (req, res) => {
+    console.log(req.session.data['edit-course'])
     res.redirect(`/applications/${req.params.applicationId}/course/edit/location`)
   })
 
   router.get('/applications/:applicationId/course/edit/location', (req, res) => {
+    let location
+    if (req.session.data['edit-course'].location) {
+      location = req.session.data['edit-course'].location
+    }
+
     res.render('applications/course/location', {
-      application: req.session.data.applications.find(app => app.id === req.params.applicationId)
+      application: req.session.data.applications.find(app => app.id === req.params.applicationId),
+      locations: getLocationItems(location)
     })
   })
 
   router.post('/applications/:applicationId/course/edit/location', (req, res) => {
+    console.log(req.session.data['edit-course'])
     res.redirect(`/applications/${req.params.applicationId}/course/edit/funding-type`)
   })
 
@@ -48,6 +91,15 @@ module.exports = router => {
   })
 
   router.post('/applications/:applicationId/course/edit/funding-type', (req, res) => {
+    console.log(req.session.data['edit-course'])
+    res.redirect(`/applications/${req.params.applicationId}/course/edit/save`)
+  })
+
+  router.get('/applications/:applicationId/course/edit/save', (req, res) => {
+
+    console.log(req.session.data['edit-course'])
+
+
     res.redirect(`/applications/${req.params.applicationId}`)
   })
 
