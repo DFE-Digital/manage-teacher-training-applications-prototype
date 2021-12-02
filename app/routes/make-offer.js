33682fad64d729e2ddc5cff5eeba6c5e5819a28c
Simon Whatley <simonwhatley@gmail.com>
2021-12-02 13:22:02 +0000

    Update new offer flow

--- app/routes/make-offer.js
+++ app/routes/make-offer.js
@@ -1,5 +1,40 @@
 const { v4: uuidv4 } = require('uuid')
 const ApplicationHelper = require('../data/helpers/application')
+const Utils = require('../data/helpers/utils')
+
+const locations = require('../data/locations')
+
+const getLocationItems = (selectedItem) => {
+  const items = []
+
+  locations.forEach((location, i) => {
+    const item = {}
+
+    item.text = location.name
+    item.value = location.id
+    item.id = location.id
+    item.checked = (selectedItem && selectedItem.includes(location.id)) ? 'checked' : ''
+
+    item.hint = {}
+    item.hint.text = Utils.arrayToList(
+        array = Object.values(location.address),
+        join = ', ',
+        final = ', '
+      )
+
+    items.push(item)
+  })
+
+  items.sort((a,b) => {
+    return a.text.localeCompare(b.text)
+  })
+
+  return items
+}
+
+const getLocation = (locationId) => {
+  return locations.find(location => location.id === locationId)
+}
 
 module.exports = router => {
 
@@ -27,18 +62,25 @@ module.exports = router => {
 
   router.get('/applications/:applicationId/offer/new/check', (req, res) => {
     let application = req.session.data.applications.find(app => app.id === req.params.applicationId)
-    var conditions = []
-    if(req.session.data['new-offer'] && req.session.data['new-offer']['standard-conditions'] && req.session.data['new-offer']['standard-conditions'].length) {
+    let conditions = []
+
+    if (req.session.data['new-offer']
+      && req.session.data['new-offer']['standard-conditions']
+      && req.session.data['new-offer']['standard-conditions'].length) {
       conditions = conditions.concat(req.session.data['new-offer']['standard-conditions'])
     }
 
-    req.session.data['new-offer']['conditions'].filter(c => c != '').forEach(c => {
-      conditions.push(c)
-    })
+    if (req.session.data['new-offer']['conditions']) {
+      req.session.data['new-offer']['conditions']
+        .filter(condition => condition != '')
+        .forEach(condition => {
+        conditions.push(condition)
+      })
+    }
 
-    conditions = conditions.map(c => {
+    conditions = conditions.map(condition => {
       return {
-        description: c,
+        description: condition,
         status: "Pending"
       }
     })
@@ -46,7 +88,8 @@ module.exports = router => {
     res.render('applications/offer/new/check', {
       upcomingInterviews: ApplicationHelper.getUpcomingInterviews(application),
       application,
-      conditions
+      conditions,
+      location: getLocation(req.session.data['new-offer'].location)
     })
   })
 
@@ -59,45 +102,53 @@ module.exports = router => {
     })
 
     application.status = 'Offered'
+
     application.offer = {
       madeDate: new Date().toISOString(),
       provider: req.session.data['new-offer'].provider || application.provider,
       course: req.session.data['new-offer'].course || application.course,
-      location: req.session.data['new-offer'].location || application.location,
+      location: getLocation(req.session.data['new-offer'].location) || application.location,
       studyMode: req.session.data['new-offer'].studyMode || application.studyMode,
-      accreditedBody: application.accreditedBody
+      accreditedBody: application.accreditedBody,
+      fundingType: req.session.data['new-offer'].fundingType || application.fundingType
     }
 
     application.offer.declineByDate = ApplicationHelper.calculateDeclineDate(application)
     application.offer.daysToDecline = ApplicationHelper.calculateDaysToDecline(application)
 
     // save standard conditions
-    if(req.session.data['new-offer'] && req.session.data['new-offer']['standard-conditions'] && req.session.data['new-offer']['standard-conditions'].length) {
-      application.offer.standardConditions = req.session.data['new-offer']['standard-conditions'].map(c => {
+    if (req.session.data['new-offer']
+      && req.session.data['new-offer']['standard-conditions']
+      && req.session.data['new-offer']['standard-conditions'].length) {
+      application.offer.standardConditions = req.session.data['new-offer']['standard-conditions'].map(condition => {
         return {
           id: uuidv4(),
-          description: c,
+          description: condition,
           status: "Pending"
         }
       })
     }
 
     // save further conditions
-    let furtherConditions = req.session.data['new-offer'].conditions.filter(c => c != '').map(c => {
-      return {
-        id: uuidv4(),
-        description: c,
-        status: "Pending"
-      }
-    })
+    if (req.session.data['new-offer']['conditions']) {
+      let furtherConditions = req.session.data['new-offer']['conditions']
+        .filter(condition => condition != '')
+        .map(condition => {
+          return {
+            id: uuidv4(),
+            description: c,
+            status: "Pending"
+          }
+        })
 
-    if(furtherConditions.length) {
-      application.offer.conditions = furtherConditions
+      if (furtherConditions.length) {
+        application.offer.conditions = furtherConditions
+      }
     }
 
     ApplicationHelper.addEvent(application, {
       title: 'Offer made',
-      user: 'Adam Davids',
+      user: req.session.data.user.firstName + ' ' + req.session.data.user.lastName,
       date: application.offer.madeDate,
       meta: {
         offer: {
@@ -118,43 +169,91 @@ module.exports = router => {
     res.redirect(`/applications/${req.params.applicationId}/offer`)
   })
 
+  // ---------------------------------------------------------------------------
+  // Change course flow
+  // ---------------------------------------------------------------------------
 
   router.get('/applications/:applicationId/offer/new/provider', (req, res) => {
-
-    const application = req.session.data.applications.find(app => app.id === req.params.applicationId)
     res.render('applications/offer/new/provider', {
-      application
+      application: req.session.data.applications.find(app => app.id === req.params.applicationId)
     })
   })
 
   router.post('/applications/:applicationId/offer/new/provider', (req, res) => {
-    res.redirect(`/applications/${req.params.applicationId}/offer/new/course`)
+    res.redirect(`/applications/${req.params.applicationId}/offer/new/course?referrer=provider`)
   })
 
   router.get('/applications/:applicationId/offer/new/course', (req, res) => {
-    const application = req.session.data.applications.find(app => app.id === req.params.applicationId)
     res.render('applications/offer/new/course', {
-      application
+      application: req.session.data.applications.find(app => app.id === req.params.applicationId)
     })
   })
 
   router.post('/applications/:applicationId/offer/new/course', (req, res) => {
-    res.redirect(`/applications/${req.params.applicationId}/offer/new/location`)
+    res.redirect(`/applications/${req.params.applicationId}/offer/new/study-mode?referrer=course`)
+  })
+
+  router.get('/applications/:applicationId/offer/new/study-mode', (req, res) => {
+    res.render('applications/offer/new/study-mode', {
+      application: req.session.data.applications.find(app => app.id === req.params.applicationId)
+    })
+  })
+
+  router.post('/applications/:applicationId/offer/new/study-mode', (req, res) => {
+    res.redirect(`/applications/${req.params.applicationId}/offer/new/location?referrer=study-mode`)
   })
 
   router.get('/applications/:applicationId/offer/new/location', (req, res) => {
-    const application = req.session.data.applications.find(app => app.id === req.params.applicationId)
+    let location
+    if (req.session.data['new-offer'] && req.session.data['new-offer'].location) {
+      location = req.session.data['new-offer'].location
+    }
+
     res.render('applications/offer/new/location', {
-      application
+      application: req.session.data.applications.find(app => app.id === req.params.applicationId),
+      locations: getLocationItems(location)
     })
   })
 
   router.post('/applications/:applicationId/offer/new/location', (req, res) => {
-    if(!req.session.data['new-offer']['standard-conditions'] || !req.session.data['new-offer'].conditions) {
+    res.redirect(`/applications/${req.params.applicationId}/offer/new/funding-type?referrer=location`)
+  })
+
+  router.get('/applications/:applicationId/offer/new/funding-type', (req, res) => {
+    res.render('applications/offer/new/funding-type', {
+      application: req.session.data.applications.find(app => app.id === req.params.applicationId)
+    })
+  })
+
+  router.post('/applications/:applicationId/offer/new/funding-type', (req, res) => {
+    if (!(req.session.data['new-offer']['standard-conditions']
+      || req.session.data['new-offer'].conditions)) {
       res.redirect(`/applications/${req.params.applicationId}/offer/new`)
     } else {
       res.redirect(`/applications/${req.params.applicationId}/offer/new/check`)
     }
+  })
+
+  router.get('/applications/:applicationId/offer/new/course/cancel', (req, res) => {
+    // delete data we don't need
+    delete req.session.data['new-offer'].provider
+    delete req.session.data['new-offer'].course
+    delete req.session.data['new-offer'].studyMode
+    delete req.session.data['new-offer'].location
+    delete req.session.data['new-offer'].accreditedBody
+    delete req.session.data['new-offer'].fundingType
+
+    if (!(req.session.data['new-offer']['standard-conditions']
+      || req.session.data['new-offer'].conditions)) {
+      res.redirect(`/applications/${req.params.applicationId}/offer/new`)
+    } else {
+      res.redirect(`/applications/${req.params.applicationId}/offer/new/check`)
+    }
+  })
+
+  router.get('/applications/:applicationId/offer/new/cancel', (req, res) => {
+    delete req.session.data['new-offer']
+    res.redirect(`/applications/${req.params.applicationId}`)
   })
 
 }
