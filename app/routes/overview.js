54c4b7fe18d426e8701ca0d75e3f41edd1c86ed4
Adam Silver <adambsilver@gmail.com>
2022-04-07 12:42:16 +0100

    Locations

--- app/routes/overview.js
+++ app/routes/overview.js
@@ -7,18 +7,22 @@ function getBreakdown(params) {
   let location = params.location
 
   let orgType = 'provider'
+  if(organisation.isAccreditedBody) {
+    orgType = 'accreditedBody'
+  }
+
+  // override
+  if(params.actingAsTrainingProvider) {
+    orgType = 'provider'
+  }
 
   let applications = params.applications
     .filter(app => app.cycle == CycleHelper.CURRENT_CYCLE.code)
     .filter(app => app[orgType] == organisation.name)
 
-  if(organisation.isAccreditedBody) {
-    orgType = 'accreditedBody'
-  }
-
   if(location) {
     applications = applications
-      .filter(app => app.location.name == location.name)
+      .filter(app => app.location && app.location.name == location.name)
   }
 
   let received = applications.filter(app => app.status == 'Received')
@@ -66,33 +70,47 @@ module.exports = router => {
 
       let activeApplicationsSection = {
         organisation: userOrganisation,
-        items: []
+        partnersTable: {
+          items: []
+        },
+        locationsTable: {
+          items: []
+        }
       }
 
+      // Create locations table
+      if(!userOrganisation.isAccreditedBody && userOrganisation.locations) {
+        userOrganisation.locations.forEach(location => {
+          activeApplicationsSection.locationsTable.items.push(getBreakdown({ organisation: userOrganisation, applications, location }))
+        })
+      }
 
+      // create partners table
+
+      // Populate the SCITT even though they are acting as a training provider for self ratified courses in this case
       if(userOrganisation.isAccreditedBody) {
         // populate the user org if it's an accredited body
-        activeApplicationsSection.items.push(getBreakdown({ organisation: userOrganisation, applications }))
+
+        activeApplicationsSection.partnersTable.items.push(getBreakdown({ organisation: userOrganisation, applications, actingAsTrainingProvider: true }))
 
         // populate any locations as rows too
         if(userOrganisation.locations) {
           userOrganisation.locations.forEach(location => {
-            activeApplicationsSection.items.push(getBreakdown({ organisation: userOrganisation, applications, location }))
+            activeApplicationsSection.partnersTable.items.push(getBreakdown({ organisation: userOrganisation, applications, location }))
           })
         }
-
       }
 
       // get partner data from relationships
       req.session.data.user.relationships.forEach(relationship => {
         // org 2 is always the partner
         var organisation = relationship.org2
-        activeApplicationsSection.items.push(getBreakdown({ organisation, applications }))
+        activeApplicationsSection.partnersTable.items.push(getBreakdown({ organisation, applications }))
 
         // populate any locations as rows too
         if(!organisation.isAccreditedBody && organisation.locations) {
           organisation.locations.forEach(location => {
-            activeApplicationsSection.items.push(getBreakdown({ organisation, applications, location }))
+            activeApplicationsSection.partnersTable.items.push(getBreakdown({ organisation, applications, location }))
           })
         }
 
