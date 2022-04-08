79d9ab3ab4f1ef4b91c8dd7f9fc914818d6dcadd
Adam Silver <adambsilver@gmail.com>
2022-04-08 09:52:34 +0100

    Reinstate locations table

--- app/routes/overview.js
+++ app/routes/overview.js
@@ -82,7 +82,7 @@ module.exports = router => {
       // Create locations table
       if(!userOrganisation.isAccreditedBody && userOrganisation.locations) {
         userOrganisation.locations.forEach(location => {
-          // activeApplicationsSection.locationsTable.items.push(getBreakdown({ organisation: userOrganisation, applications, location }))
+          activeApplicationsSection.locationsTable.items.push(getBreakdown({ organisation: userOrganisation, applications, location }))
         })
       }
 
