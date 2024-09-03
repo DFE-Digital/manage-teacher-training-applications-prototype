2de438e6307a6785dea1a36a1e12046df04c1b03
Pete Williams <me@petewilliams.info>
2024-09-03 14:15:50 +0100

    Update app/routes.js
    Co-authored-by: Simon Whatley <23444+simonwhatley@users.noreply.github.com>

--- app/routes.js
+++ app/routes.js
@@ -39,13 +39,14 @@ require('./routes/withdraw-offer')(router)
 require('./routes/examples')(router)
 
 // viewing session data
-router.get('*/manage-prototype/view-data', function(req, res){
-
-    querystring = '';
-    for ( var key in req.session.data )
-    {
-        querystring += key +'=' + req.session.data[key] + '&';
-    }
-
-    res.render('manage-prototype/view-data', { data: JSON.stringify( req.session, null, 2), querystring: querystring } );
+router.get('*/manage-prototype/view-data', (req, res) => {
+  let querystring = ''
+  for (let key in req.session.data) {
+    querystring += key + '=' + req.session.data[key] + '&'
+  }
+  
+  res.render('manage-prototype/view-data', { 
+    data: JSON.stringify(req.session, null, 2), 
+    querystring
   })
+})
