eb97d5e7269a767d4e4c18a4ef917e63b2a99d69
Pete Williams <me@petewilliams.info>
2026-02-10 11:38:15 +0000

    Adding interview preference as organisational setting

--- app/routes/organisation-settings.js
+++ app/routes/organisation-settings.js
@@ -18,4 +18,54 @@ module.exports = router => {
     })
   })
 
+  
+  router.get('/organisation-settings/:orgId/interview-preferences', (req, res) => {
+    let org = req.session.data.organisations.find(org => org.id == req.params.orgId)
+    let users = req.session.data.users.filter(user => {
+      return user.organisation.id == req.params.orgId
+    })
+
+    users.sort((a, b) => a.firstName.localeCompare(b.firstName) || a.lastName.localeCompare(b.lastName))
+
+    res.render('organisation-settings/interview-preferences/index', {
+      org,
+      users
+    })
+  })
+
+  
+  router.get('/organisation-settings/:orgId/interview-preferences/edit', (req, res) => {
+    let org = req.session.data.organisations.find(org => org.id == req.params.orgId)
+    let users = req.session.data.users.filter(user => {
+      return user.organisation.id == req.params.orgId
+    })
+
+    users.sort((a, b) => a.firstName.localeCompare(b.firstName) || a.lastName.localeCompare(b.lastName))
+
+    res.render('organisation-settings/interview-preferences/edit', {
+      org,
+      users
+    })
+  })
+
+  router.post('/organisation-settings/:orgId/interview-preferences', (req, res) => {
+    let showBanner = true
+    let org = req.session.data.organisations.find(org => org.id == req.params.orgId)
+    let users = req.session.data.users.filter(user => {
+      return user.organisation.id == req.params.orgId
+    })
+
+    users.sort((a, b) => a.firstName.localeCompare(b.firstName) || a.lastName.localeCompare(b.lastName))
+
+
+    req.session.data.user.organisation['interviewPref'] = req.session.data['interviewPref']
+    res.render('organisation-settings/interview-preferences/index', {
+      org,
+      users,
+      showBanner
+    })
+    delete req.session.data['interviewPref']
+  })
+
+
 }
